97c13cd4c383be6c3d1c6af8d6925dc943ac6ef8
janpihlgren <janpihlgren>
2003-05-18 06:05:27 +0000

    Uppdaterad funktion

--- prototype/src/Kontorapport/ktorapport.ui.h
+++ prototype/src/Kontorapport/ktorapport.ui.h
@@ -8,8 +8,9 @@
 /***************************************************************************
                           RPTKTOW  -  description
                              -------------------
-		     version 0.1
-    begin                : Lör 8 mars 2003
+		     version 0.2
+    begin                	: Lör 8 mars 2003
+    update		:Tors 15 maj 2003
     copyright            : (C) 2003 by Jan Pihlgren
     email                : jan@pihlgren.se
  ***************************************************************************/
@@ -34,7 +35,7 @@
 #define MAXSTRING 15000
 
     QProcess* process;
-    QString inrad;
+    QString inrad="";
     QString* rad;
     QString errorrad;
     int status = 0;
@@ -56,6 +57,7 @@ void frmKtoRapport::init()
 void frmKtoRapport::slotLineEditBar_returnPressed()
 {
     status=0;
+    arid="";
     ListView1->clear();
     arid=LineEditBar->text();
     arid=arid.upper();
@@ -76,7 +78,7 @@ void frmKtoRapport::slotGetVerrader()
 {
 	const char *userp = getenv("USER");
             QString usr(userp);
-
+	    inrad="";
 	process = new QProcess();
 	process->addArgument("./STYRMAN");	// OLFIX styrprogram
 	process->addArgument(usr);		// userid
@@ -93,7 +95,6 @@ void frmKtoRapport::slotGetVerrader()
 	    QMessageBox::warning( this, "RPTKTOW",
                             "Kan inte starta STYRMAN/BARCHK! \n" );
 	    }
-
 }
 
 
@@ -160,7 +161,6 @@ void frmKtoRapport::slotDataOnStdout()
     }
 }
 
-
 void frmKtoRapport::slotDataOnStderr()
 {
     while (process->canReadLineStderr() ) {
@@ -170,34 +170,32 @@ void frmKtoRapport::slotDataOnStderr()
     }
 }
 
-
 void frmKtoRapport::slotKTORPTEndOfProcess()
 {
   QListViewItem* item;
+  QString KontoNr;
+  QString tmpKtoNr;
+  QString KontoNamn;
+  QString tmpKtoNamn;
+  QString DK;
+  QString BELOPP;
   QString Db;
   QString Kr;
   QString Sum;
+  QString Total;
   QString DEBET;
   QString KREDIT;
-  int i;
+  
+  int i,antrad;
   int j,k,l,m;
-  char *pos1;
-  char *pos2;
-  char tmp[MAXSTRING];
-  char *tmppek;
-  char antrad[6]="";
-  char ktonr[5]="";
-  char tmpktonr[5]="";
-  char debet[13]="";
-  char kredit[13]="";
-  char belopp[13]="";
-  double debetsumma=0;
-  double kreditsumma=0;
-  double delsummadb=0;
-  double delsummakr=0;
+  
+  double dbbelopp=0;
+  double dbtotal=0;
+  double krbelopp=0;
+  double krtotal=0;
   double summa=0;
-  char dk[2]="";
-
+  double total=0;
+   
     i = -1;
     i = errorrad.find( QRegExp("Error:"), 0 );
     if (i != -1) {
@@ -206,148 +204,99 @@ void frmKtoRapport::slotKTORPTEndOfProcess()
 	 errorrad="";
 	 arid="";
 	 LineEditBar->setFocus();
-     }else{
-//	 inradktolist.latin1();
-//	 qDebug("inradktolist=%s\n",inradktolist.latin1());
-//	 qDebug("inrad=%s\n",inrad.latin1());
-	 tmppek=tmp;
-	 qstrcpy(tmp,inrad);
-	 pos1=strstr(tmp,"NR_");
-	 pos2=strstr(tmp,"_:");
-	 i = pos2 - pos1;
-	 m=i+2;		// startposition för första ktonr.
-//	 qDebug("i=%d m=%d\n",i,m);
-	 k=0;
-	 for (j=3;j<i;j++){
-	     antrad[k]=tmp[j];
-	     k++;
-	 };
-	 i = atoi(antrad);		// i = antal poster
-	 for (k = 1;k <= i; k++){	// gå igenom alla raderna / posterna
-	     l=0;
-	     for(j = m; j < sizeof(ktonr) + m; j++){
-		 if(tmp[j] != *("_")){
-		     ktonr[l]=tmp[j];
-		     l++;
-		  }else{
-		      ktonr[l] = *("\0");
-		      j=sizeof(ktonr) + m;
-		  }
-	      }
-	     if (strlen(tmpktonr) == 0 ){
-		 strcpy(tmpktonr,ktonr);
-	     }
-//	     fprintf(stdout,"tmpktonr=%s ktonr=%s\n ",tmpktonr,ktonr);
-	     m=m+l+2;	// position för dk
-	     l=0;
-	     for(j = m; j < sizeof(dk) + m; j++){
-		 if(tmp[j] != *("_")){
-		     dk[l]=tmp[j];
-		     l++;
-		  }else{
-		      dk[l] = *("\0");
-		      j=sizeof(dk) + m;
-		  }
-	      }
-		 //	fprintf(stdout,"%s  ",dk);
-	     m=m+l+2;	// position för belopp
-	     l=0;
-	     for(j = m; j < sizeof(belopp) + m; j++){
-		 if(tmp[j] != *("_")){
-		     belopp[l]=tmp[j];
-		     l++;
-		  }else{
-		      belopp[l] = *("\0");
-		      j=sizeof(belopp) + m;
-		  }
-	      }
-		 //	fprintf(stdout,"%s  ",belopp);
-	     m=m+l+2;
-	     if (strcmp(tmpktonr,ktonr) != 0){
-		 summa=delsummadb-delsummakr;
-		Db = Db.setNum(delsummadb,'f',2);
-		Kr = Kr.setNum(delsummakr,'f',2);
-		Sum=Sum.setNum(summa,'f',2);
-		while (Db.length() < 13){
-		    Db.prepend(" ");
-		}
-		while (Kr.length() < 13){
-		    Kr.prepend(" ");
-		}
-		while (Sum.length() < 13){
-		    Sum.prepend(" ");
-		}
-		item = new QListViewItem(ListView1,tmpktonr,"Delsumma",Db,Kr,Sum);
-		delsummadb=0;
-		delsummakr=0;
-		strcpy(tmpktonr,ktonr);
-	     }
-	     
-	     if (strcmp(dk,"D") ==0){
-		 strcpy(debet,belopp);
-		 strcpy(kredit,"");
-		 Db=belopp;
-		 debetsumma=debetsumma+Db.toDouble();
-		 delsummadb=delsummadb+Db.toDouble();
-	     }else{
-		 strcpy(debet,"");
-		 strcpy(kredit,belopp);
-		 Kr=belopp;
-		 kreditsumma=kreditsumma+Kr.toDouble();
-		 delsummakr=delsummakr+Kr.toDouble();
-	     }
-	     DEBET=debet;
-	     KREDIT=kredit;
-	     while (DEBET.length() <13){
-		 DEBET.prepend(" ");
-	     }
-	     while (KREDIT.length() < 13){
-		 KREDIT.prepend(" ");
-	     }
-//	     item = new QListViewItem(ListView1,ktonr,"",debet,kredit,"");
-	    item = new QListViewItem(ListView1,ktonr,"",DEBET,KREDIT,"");
-		 /* rensa ktonr,dk,belopp */
-	     for (l=0;l<sizeof(ktonr);l++){
-		 ktonr[l]=*("\0");
-	     }
-	     for (l=0;l<sizeof(dk);l++){
-		     dk[l]=*("\0");
-	     }
-     	     for (l=0;l<sizeof(belopp);l++){
-		     belopp[l]=*("\0");
-	     }
-	 }
      }
-    summa=delsummadb-delsummakr;
-    Db = Db.setNum(delsummadb,'f',2);
-    Kr = Kr.setNum(delsummakr,'f',2);
-    Sum=Sum.setNum(summa,'f',2);    
-    while (Db.length() < 13){
-	Db.prepend(" ");
-    }
-    while (Kr.length() < 13){
-	Kr.prepend(" ");
-    }
-    while (Sum.length() < 13){
-	Sum.prepend(" ");
-    }
-    if (strlen(tmpktonr) !=0){
-	item = new QListViewItem(ListView1,tmpktonr,"Delsumma",Db,Kr,Sum);
-    }    
-    summa=debetsumma-kreditsumma;
-     Db = Db.setNum(debetsumma,'f',2);
-     Kr = Kr.setNum(kreditsumma,'f',2);
-     Sum=Sum.setNum(summa,'f',2); 
-     while (Db.length() < 13){
-	 Db.prepend(" ");
-     }
-     while (Kr.length() < 13){
-	 Kr.prepend(" ");
-     }
-     while (Sum.length() < 13){
-	 Sum.prepend(" ");     
-     }
-     if (debetsumma != 0){
-	  item = new QListViewItem(ListView1,"Total","Summa",Db,Kr,Sum);  
-      }	 
+//    qDebug("inrad=%s",inrad.latin1());
+    i = inrad.find(QRegExp("NR_"),0);
+    j = inrad.find(QRegExp("_:"),0);
+    m = j - i;
+//   qDebug("i=%d, j=%d, m=%d",i,j,m); 
+   antrad = (inrad.mid(i+3,m-3)).toInt();
+//   qDebug("antrad=%d,",antrad);
+   tmpKtoNr="";
+   
+   for (l = 0;l < antrad; l++){
+//      qDebug("l=%d,",l);    
+       k = inrad.find(QRegExp("_:"),j+3);  // j+2
+       m = k - j;
+       KontoNr = inrad.mid(j+2,m-2);
+//       qDebug("l=%d, KontoNr=%s",l,KontoNr.latin1());
+       
+       j = k;
+       k = inrad.find(QRegExp("_:"),j+2);
+       m = k - j;
+       KontoNamn =inrad.mid(j+2,m-2);
+//      qDebug("KontoNamn=%s",KontoNamn.latin1());
+   
+       j = k;
+       k = inrad.find(QRegExp("_:"),j+2);
+       m = k - j;
+       DK =inrad.mid(j+2,m-2);
+//       qDebug("DK=%s",DK.latin1());    
+
+       j = k;
+       k = inrad.find(QRegExp("_:"),j+2);
+       m = k - j;
+       BELOPP = inrad.mid(j+2,m-2);
+//       qDebug("BELOPP=%s",BELOPP.latin1());    
+      
+      if ((KontoNr == tmpKtoNr) && (KontoNr !=""))  {
+	  if (DK == "D"){
+	      DEBET=BELOPP;
+	      dbbelopp=dbbelopp+BELOPP.toDouble();
+                  dbtotal=dbtotal+BELOPP.toDouble();
+	      KREDIT="";
+	  }
+	  if (DK == "K"){
+	      KREDIT=BELOPP;
+	      krbelopp=krbelopp+BELOPP.toDouble();
+	      krtotal=krtotal+BELOPP.toDouble();
+	      DEBET="";
+	  }
+              summa=dbbelopp-krbelopp;
+  	  item = new QListViewItem(ListView1,KontoNr,"z",DEBET,KREDIT,"");    	       
+//	  total=total+summa;
+      }else{
+              Sum.setNum(summa,'f',2);
+	  if (tmpKtoNr !=""){
+	      item = new QListViewItem(ListView1,tmpKtoNr,"z       Delsumma","","",Sum);
+	  }
+	  summa=0; 
+	  dbbelopp=0;
+	  krbelopp=0;
+	  
+	  if (DK == "D"){
+	      DEBET=BELOPP;
+	      dbbelopp=dbbelopp+BELOPP.toDouble();
+                  dbtotal=dbtotal+BELOPP.toDouble();
+	      KREDIT="";
+	  }
+	  if (DK == "K"){
+	      KREDIT=BELOPP;
+	      krbelopp=krbelopp+BELOPP.toDouble();
+	      krtotal=krtotal+BELOPP.toDouble();	      
+	      DEBET="";
+	  }
+              summa=dbbelopp-krbelopp;
+//              total=total+summa;
+	  item = new QListViewItem(ListView1,KontoNr,KontoNamn,DEBET,KREDIT,"");      
+      }
+       tmpKtoNr=KontoNr;
+         
+       j = k;
+   };
+   
+  Sum.setNum(summa,'f',2);
+  item = new QListViewItem(ListView1,tmpKtoNr,"z       Delsumma","","",Sum);
+
+   Db.setNum(dbtotal,'f',2);
+   Kr.setNum(krtotal,'f',2);
+   
+   total=dbtotal-krtotal; 
+   Total.setNum(total,'f',2);
+   item = new QListViewItem(ListView1,"Total","",Db,Kr,Total); 
+   
+//	    item = new QListViewItem(ListView1,ktonr,KontoNamn,DEBET,KREDIT,"");
+//    if (strlen(tmpktonr) !=0){
+//	item = new QListViewItem(ListView1,tmpktonr,"Delsumma",Db,Kr,Sum);
+//	  item = new QListViewItem(ListView1,"Total","Summa",Db,Kr,Sum);  
 }
