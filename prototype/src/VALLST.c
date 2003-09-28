5a05d553b7795b3e193a7512ba8b1dc6932197b8
janpihlgren <janpihlgren>
2003-09-28 04:53:45 +0000

    Uppdaterat VALLST.c

--- prototype/src/VALLST.c
+++ prototype/src/VALLST.c
@@ -1,7 +1,8 @@
 /***************************************************************************
-                          VALRLST.c  -  description
+                          VALLST.c  -  description
                              -------------------
-    begin                : Tors 20 febr
+    begin                : Sön 1 juni
+    Version		 : 0.02
     copyright            : (C) 2003 by Jan Pihlgren
     email                : jan@pihlgren.se
  ***************************************************************************/
@@ -23,7 +24,7 @@
 
 */
  /*@unused@*/ static char RCS_id[] =
-    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/VALLST.c,v 1.1 2003/05/08 08:54:13 frazze Exp $ " ;
+    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/VALLST.c,v 1.2 2003/09/28 04:53:45 janpihlgren Exp $ " ;
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -31,24 +32,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+//#include <unistd.h>
+//#include <string.h>
+//#include <ctype.h>
 #include "mysql.h"
+#define HOST 100
 
   MYSQL my_connection;
   MYSQL_RES *res_ptr;
   MYSQL_ROW sqlrow;
 
+	char tmphost[HOST];
+	char host[15];
+	char databas[15];
+	char databasuser[15];
+	char losenord[15];
 
+int find_host(char *envp[]);
 void display_row();
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char *envp[])
 {
-  int res,i;
+  int res,i,status;
 
+  status = find_host(envp);
+  if (status != 0)
+	exit(status);
+//  fprintf(stderr,"host=%s\n",host);
+//  fprintf(stderr,"VALLST\n");
   mysql_init(&my_connection);
 
-  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", "olfix", 0, NULL, 0)){
-//  fprintf(stderr,"USERLST:Connection success\n");
+
+//  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", "olfix", 0, NULL, 0)){
+  if (mysql_real_connect(&my_connection, host,  databas, databasuser, losenord, 0, NULL, 0)){
+//  fprintf(stderr,"VALLST:Connection success\n");
 
   res = mysql_query(&my_connection,"SELECT * FROM VALUTA ORDER BY VALUTAID");
 
@@ -97,3 +114,65 @@ void display_row()
 //	fprintf(stdout,"\n");
 }
 
+int find_host(char *envp[])
+{
+	FILE *fil_pek;
+
+	char home[50];
+	char *home_pek;
+	char resource[]="/.olfixrc";
+	char filename[50]="";
+	char tmp[50]="";
+	char temp[10]="";
+	char *tmp_pek;
+	char *tmp_pek2;
+	int i,status;
+
+	for (i = 0;envp[i]!=NULL;i++){
+		if(strstr(envp[i],"HOME=") != NULL){
+			strncpy(temp,envp[i],4);
+			status=strcmp(temp,"HOME");
+			if (status == 0){
+				home_pek=(strstr(envp[i],"HOME="));
+				home_pek=home_pek+5;
+				strcpy(home,home_pek);
+			}
+		}
+	}
+	strcpy(filename,home);
+	strcat(filename,resource);
+
+	status=-1;
+
+	if ((fil_pek = fopen(filename,"r")) != NULL){
+		while (fgets(tmp,50,fil_pek) != NULL){
+			if(strstr(tmp,"HOST=")){
+			//	tmp_pek=(strstr(tmp,"HOST="))+5;
+			//	fprintf(stderr,"tmp_pek=%d\n",tmp_pek);
+			//	strcpy(tmphost,tmp_pek);
+			//	fprintf(stderr,"tmphost=%s\n",tmphost);
+				tmp_pek=(strstr(tmp,"1:"))+2;
+				tmp_pek2=strstr(tmp,"2:");
+				strncpy(host,tmp_pek,(tmp_pek2-tmp_pek)-1);
+			//	fprintf(stderr,"host=%s\n",host);
+				tmp_pek=(strstr(tmp,"2:"))+2;										tmp_pek2=strstr(tmp,"3:");
+				strncpy(databas,tmp_pek,(tmp_pek2-tmp_pek)-1);
+			//	fprintf(stderr,"databas=%s längd=%d\n",databas,strlen(databas));
+				tmp_pek=(strstr(tmp,"3:"))+2;
+				tmp_pek2=strstr(tmp,"4:");
+				strncpy(databasuser,tmp_pek,(tmp_pek2-tmp_pek)-1);
+			//	fprintf(stderr,"user=%s längd=%d\n",databasuser,strlen(databasuser));
+				tmp_pek=(strstr(tmp,"4:"))+2;
+				tmp_pek2=strstr(tmp,"end");
+				strncpy(losenord,tmp_pek,(tmp_pek2-tmp_pek)-1);
+			//	fprintf(stderr,"losen=%s längd=%d\n",losenord,strlen(losenord));
+				status=0;
+			}
+		}
+		fclose(fil_pek);
+	}
+	else{
+	 	fprintf(stderr,"Error: Filen .olfixrc kan inte öppnas\n");
+	}
+	return status;
+}
