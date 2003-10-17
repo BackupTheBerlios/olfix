1b888a65462ea9f816654b613c0ee3078d25faf5
janpihlgren <janpihlgren>
2003-10-17 07:52:15 +0000

    Tilllägg av testföretag

--- prototype/src/FTGDSP.c
+++ prototype/src/FTGDSP.c
@@ -1,10 +1,11 @@
 /***************************************************************************
                           FTGDSP.c  -  description
                              -------------------
-    begin                : Tis  19  nov	2002
-    modified		 : Tis 15 april 2003
-    copyright            : (C) 2002 by Jan Pihlgren
-    email                : jan@pihlgren.se
+    Version		: 0.2
+    begin               : Tis 19 nov 2002
+    modified		: Tis 14 okt 2003
+    copyright           : (C) 2002 by Jan Pihlgren
+    email               : jan@pihlgren.se
  ***************************************************************************/
 
 /***************************************************************************
@@ -17,9 +18,9 @@
  *********************************************** ****************************/
 
 /*
-	INPUT: 1 st arg = posttyp
+	INPUT: 2 st arg = posttyp database
 
-	Kommando: ./FTGDSP posttyp
+	Kommando: ./FTGDSP posttyp database
 
 	Function: Visa information för önskad posttyp
 
@@ -28,7 +29,7 @@
 */
 
  /*@unused@*/ static char RCS_id[] =
-    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/FTGDSP.c,v 1.1 2003/05/08 08:54:06 frazze Exp $ " ;
+    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/FTGDSP.c,v 1.2 2003/10/17 07:52:15 janpihlgren Exp $ " ;
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -48,63 +49,98 @@ int main(int argc, char *argv[])
 {
   MYSQL my_connection;
   int res;
+  int num_rows;
 //  int i;
 
-  char temp1[]="SELECT POSTTYP, FDATA FROM FTGDATA WHERE POSTTYP = \"";
+  const char *userp = getenv("USER");	// vem är inloggad?
+
+  char databas[]="olfix";
+  char temp1[]="SELECT POSTTYP, FDATA FROM FTGDATA WHERE (POSTTYP = \"";
   char temp2[]="\"";
-  char temp3[]=",";
+//  char temp3[]=",";
   char temp4[]=")";
   char temp5[200]="";
   char posttyp[5];
-
+  char fornr[4];	// företag nr fornr, 99 = testföretag
+  char usr[15];		// userid
+//  fprintf(stderr,"FTGDSPmain argc = %d\n",argc);
 //  for (i=0;i< argc;i++){
-//  	fprintf(stderr,"FTGDSPmain argv%d = %s\n",i,argv[i]);
+//	fprintf(stderr,"FTGDSPmain argv%d = %s\n",i,argv[i]);
 //  }
+//  fprintf(stderr,"FTGDSPmain argv[2] = %s\n",argv[2]);
 
+  strcpy(usr,userp);			// Den inloggades userid
+//  fprintf(stderr,"FTGDSPmain usr = %s\n",usr);
 
+  if (argc<3){
+  	strcpy(databas,"olfixtst");	// olfixtst = testföretag
+  }else{
+  	if (strncmp(argv[2],"99",2)==0){
+		strcpy(databas,"olfixtst");
+	}else{
+  		strcpy(databas,argv[2]);
+	}
+  }
+  /* Om usr (userid) börjar på 'test' eller 'prov' använd databas 'olfixtst' */
+  if (strncmp(usr,"test",4)==0 || strncmp(usr,"prov",4)==0 ) {
+  	strcpy(databas,"olfixtst");
+  }
+
+  if (argc<4){
+  	strcpy(fornr,"99");	// 99 = testföretag
+  }else{
+  	strcpy(fornr,argv[3]);
+  }
+//  fprintf(stderr,"FTGDSPmain fornr = %s\n",fornr);
   strcpy(posttyp,argv[1]);
 
   strcat(temp5,temp1);
-/* SELECT POSTTYP, FDATA FROM FTGDATA WHERE POSTTYP = \"   */
+/* SELECT POSTTYP, FDATA FROM FTGDATA WHERE (POSTTYP = \"   */
   strcat(temp5,posttyp);/* ADR1 */
-/* SELECT POSTTYP, FDATA FROM FTGDATA WHERE POSTTYP = "ADR1   */
-  strcat(temp5,temp2); /*  "     */
-/* SELECT POSTTYP, FDATA FROM FTGDATA WHERE POSTTYP = "ADR1"   */
+/* SELECT POSTTYP, FDATA FROM FTGDATA WHERE (POSTTYP = "ADR1   */
+  strcat(temp5,temp2); /*  "    */
+/* SELECT POSTTYP, FDATA FROM FTGDATA WHERE (POSTTYP = "ADR1"   */
+  strcat(temp5,temp4); /*  )     */
+/* SELECT POSTTYP, FDATA FROM FTGDATA WHERE (POSTTYP = "ADR1")  */
+
 //  fprintf(stderr,"FTGDSPmain: temp5 = %s\n",temp5);
 
+// exit(0);
   mysql_init(&my_connection);
-  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", "olfix", 0, NULL, 0)){
-//  fprintf(stdout,"FTGDSPmain:Connection success\n");
+  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", databas, 0, NULL, 0)){
+//  	fprintf(stdout,"FTGDSPmain:Connection success\n");
+  	res = mysql_query(&my_connection,temp5);
+  	if (res){
+		fprintf(stderr,"Error: FTGDSP SELECT error: %s\n",mysql_error(&my_connection));
+        }
+	else{
+		res_ptr=mysql_store_result(&my_connection);
+		num_rows = mysql_affected_rows(&my_connection);
+//		if (res_ptr){
+		if(num_rows != 0){
+			sqlrow=mysql_fetch_row(res_ptr);
+//	 		fprintf(stdout,"Fetched data....}");
+			fprintf(stdout,"OK: 1:%s  2:%s",sqlrow[0],sqlrow[1]);
+//			fprintf(stdout,"\n");
+		}
+		else{
+			fprintf(stderr,"Error: FTGDSP, Data saknas!  %s\n", mysql_error(&my_connection));
+		}
 
-  res = mysql_query(&my_connection,temp5);
-
-  if (res){
-	fprintf(stderr,"Error: FTGDSP SELECT error: %s\n",mysql_error(&my_connection));
-        }else{
-	res_ptr=mysql_store_result(&my_connection);
-	if (res_ptr){
-		sqlrow=mysql_fetch_row(res_ptr);
-//	 	fprintf(stdout,"Fetched data....}");
-		fprintf(stdout,"OK: 1:%s  2:%s",sqlrow[0],sqlrow[1]);
-//		fprintf(stdout,"2:%s  ",sqlrow[1]);
-//		fprintf(stdout,"3:%s  ",sqlrow[2]);
-//		fprintf(stdout,"4:%s  ",sqlrow[3]);
-//		fprintf(stdout,"\n");
-	if (mysql_errno(&my_connection))  {
-		fprintf(stderr,"Error: FTGDSP Retriev error:  %s\n", mysql_error(&my_connection));
+		if (mysql_errno(&my_connection))  {
+			fprintf(stderr,"Error: FTGDSP Retriev error:  %s\n", mysql_error(&my_connection));
 		}
 	}
 	mysql_free_result(res_ptr);
-	}
 	mysql_close(&my_connection);
-
-  	} else {
-    	fprintf(stderr,"Error: FTGDSP Connection failed\n");
-    	if (mysql_errno(&my_connection))   {
-    		fprintf(stderr,"Error: FTGDSP Connection error %d:  %s\n",
+  	}
+	else {
+    		fprintf(stderr,"Error: FTGDSP Connection failed\n");
+    		if (mysql_errno(&my_connection))   {
+    			fprintf(stderr,"Error: FTGDSP Connection error %d:  %s\n",
 			mysql_errno(&my_connection), mysql_error(&my_connection));
-	}
-    }
+		}
+    	}
   return EXIT_SUCCESS;
 }
 
