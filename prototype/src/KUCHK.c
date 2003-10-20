e76e597f13b08c4b97d4561e9f5638a0ca0c1c79
janpihlgren <janpihlgren>
2003-10-20 14:29:35 +0000

    Infört val av företag/databas

--- prototype/src/KUCHK.c
+++ prototype/src/KUCHK.c
@@ -2,6 +2,7 @@
                           KUCHK.c  -  description
                              -------------------
     begin                : Tis 9 sept	2003
+    Modified		 : Mån 20 okt   2003
     copyright            : (C) 2003 by Jan Pihlgren
     email                : jan@pihlgren.se
  ***************************************************************************/
@@ -26,7 +27,7 @@
 
 */
  /*@unused@*/ static char RCS_id[] =
-    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/KUCHK.c,v 1.1 2003/09/28 04:25:11 janpihlgren Exp $ " ;
+    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/KUCHK.c,v 1.2 2003/10/20 14:29:35 janpihlgren Exp $ " ;
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -40,18 +41,22 @@
   MYSQL my_connection;
   MYSQL_RES *res_ptr;
   MYSQL_ROW sqlrow;
+  void display_row();
+  int which_database(char *envp[]);
 
-int main(int argc, char *argv[])
+  char database[15]="";
+
+int main(int argc, char *argv[], char *envp[])
 {
-  MYSQL my_connection;
   int res,status;
 
   char temp1[]="SELECT KUNDNR FROM KUNDREG WHERE KUNDNR = \"";
   char temp2[]="\"";
-//  char temp3[]=",";
-//  char temp4[]=")";
   char temp5[200]="";
   char kundnr[11];
+  const char *userp = getenv("USER");	// vem är inloggad?
+  char databas[25]="olfix";
+  char usr[15];		// userid
 
   if (argv[1] != NULL){
   	strcpy(kundnr,argv[1]);
@@ -59,6 +64,38 @@ int main(int argc, char *argv[])
   	fprintf(stderr,"Error: KUCHK: Ange kundnummer!\n");
 	exit(-1);
   }
+
+  status = which_database(envp);
+
+  if (status != 0)
+	exit(status);
+
+  strcpy(usr,userp);			// Den inloggades userid
+
+  if (argc<3){
+    	if (strlen(database)!= 0){
+		strcpy(databas,database);
+	}else{
+  		strcpy(databas,"olfixtst");	// olfixtst = testföretag
+	}
+  }else{
+	if (strlen(argv[2]) != 0){
+  		if (strncmp(argv[2],"99",2)==0){
+			strcpy(databas,"olfixtst");
+		}else{
+  			strcpy(databas,argv[2]);
+  		}
+  	}
+  }
+  /* Om usr (userid) börjar på 'test' eller 'prov' använd databas 'olfixtst' */
+  if (strncmp(usr,"test",4)==0 || strncmp(usr,"prov",4)==0 ) {
+  	strcpy(databas,"olfixtst");
+  }
+//  fprintf(stderr,"KUDSP database = %s databas=%s\n",database,databas);
+
+// ================================================================================
+// 		Val av databas, END!
+// ================================================================================
 
   strcpy(temp5,temp1);
 /* SELECT KUNDNR FROM KUNDREG WHERE KUNDNR = "  */
@@ -69,7 +106,7 @@ int main(int argc, char *argv[])
 //  fprintf(stderr,"KUCHKmain: temp5 = %s\n",temp5);
 
   mysql_init(&my_connection);
-  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", "olfix", 0, NULL, 0)){
+  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", databas, 0, NULL, 0)){
 //  fprintf(stdout,"KUCHKmain:Connection success\n");
   	res = mysql_query(&my_connection,temp5);
   	if (res){
@@ -107,5 +144,66 @@ if(status != 0){
 	fprintf(stderr,"Error: KUCHK Kundnr %s finns inte!\n",kundnr);
 }
   return status;
+}
+int which_database(char *envp[])
+{
+	FILE *fil_pek;
+
+//	char home[]="$HOME";
+	char home[50];
+	char *home_pek;
+	char resource[]="/.olfixrc";
+	char filename[50]="";
+	char tmp[20]="";
+	char temp[10]="";
+	char *tmp_pek;
+	int i,status;
+
+	for (i = 0;envp[i]!=NULL;i++){
+		if(strstr(envp[i],"HOME=") != NULL){
+			strncpy(temp,envp[i],4);
+//			fprintf(stderr,"temp=%s\n",temp);
+			status=strcmp(temp,"HOME");
+//			fprintf(stderr,"status=%d\n",status);
+			if (status == 0){
+				home_pek=(strstr(envp[i],"HOME="));
+				home_pek=home_pek+5;
+				strcpy(home,home_pek);
+			}
+//			fprintf(stderr,"home_pek=%d %s\n",home_pek,home_pek);
+//			fprintf(stderr,"home_pek=%d %s\n",home_pek,home_pek);
+		}
+	}
+//	fprintf(stderr,"home=%s\n",home);
+	strcpy(filename,home);
+	strcat(filename,resource);
+
+//	fprintf(stderr,"filename=%s\n",filename);
+	status=-1;
+
+	if ((fil_pek = fopen(filename,"r")) != NULL){
+		while (fgets(tmp,150,fil_pek) != NULL){
+//			fprintf(stderr,"tmp=%s\n",tmp);
+			if(strstr(tmp,"DATABASE=")){
+				tmp_pek=(strstr(tmp,"DATABASE="))+9;
+				strncpy(database,tmp_pek,strlen(tmp_pek));
+				status=0;
+			}
+		}
+//		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database));
+		fclose(fil_pek);
+	}
+	else{
+	 	fprintf(stderr,"Error: Filen .olfixrc kan inte öppnas\n");
+	}
+	for (i=0;i < strlen(database);i++){
+		tmp[i]=database[i];
+	}
+	tmp[i-1]=0;
+//	fprintf(stderr,"tmp=%s, i=%d len tmp=%d\n",tmp,i,strlen(tmp));
+	strncpy(database,tmp,strlen(tmp));
+	database[strlen(tmp)]=0;
+
+	return status;
 }
 
