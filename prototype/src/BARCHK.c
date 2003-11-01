5305cd3b39abf0856034b05c6e034884e047fbd0
janpihlgren <janpihlgren>
2003-11-01 16:31:26 +0000

    Lagt till användandet av testföretag

--- prototype/src/BARCHK.c
+++ prototype/src/BARCHK.c
@@ -1,7 +1,9 @@
 /***************************************************************************
                           BARCHK.c  -  description
                              -------------------
+    Version		 : 0.2
     begin                : Tors 27 febr	2003
+    modified		 : Lör 1 nov	2003
     copyright            : (C) 2003 by Jan Pihlgren
     email                : jan@pihlgren.se
  ***************************************************************************/
@@ -26,7 +28,7 @@
 
 */
  /*@unused@*/ static char RCS_id[] =
-    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/BARCHK.c,v 1.1 2003/05/08 08:54:04 frazze Exp $ " ;
+    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/BARCHK.c,v 1.2 2003/11/01 16:31:26 janpihlgren Exp $ " ;
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -35,46 +37,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "mysql.h"
+#define ANTARG 2
+
   MYSQL my_connection;
   MYSQL_RES *res_ptr;
   MYSQL_ROW sqlrow;
 
-int main(int argc, char *argv[])
+int which_database(char *envp[]);
+
+  char database[15]="";
+
+int main(int argc, char *argv[], char *envp[])
 {
-  MYSQL my_connection;
-  int res,status;
+  int res;
+  int status;
+  const char *userp = getenv("USER");	/* vem är inloggad?	*/
+  char databas[25]="olfix";
+  char usr[15];				/* userid		*/
 
   char temp1[]="SELECT ARID FROM BOKFAR WHERE ARID = \"";
   char temp2[]="\"";
-  char temp3[]=",";
-  char temp4[]=")";
   char temp5[200]="";
-  char arid[8];
-  char vernr[10];
+  char arid[3]="";
 
-  strcpy(arid,argv[1]);
+  /* ================================================================================ */
+/* 		Val av databas, START						    */
+/* ================================================================================ */
 
-  strcat(temp5,temp1);
+  status = which_database(envp);
+
+  if (status != 0)
+	exit(status);
+
+  strncpy(usr,userp,15);			// Den inloggades userid
+/*  fprintf(stderr,"status=%d ANTARG=%d len(database)=%d\n",status,ANTARG,strlen(database));	*/
+  if (argc < ANTARG+1){
+    	if (strlen(database)!= 0){
+		strncpy(databas,database,15);
+	}else{
+  		strncpy(databas,"olfixtst",15);	// olfixtst = testföretag
+	}
+  }else{
+	if (strlen(argv[ANTARG]) != 0){
+  		if (strncmp(argv[ANTARG],"99",2)==0){
+			strncpy(databas,"olfixtst",15);
+		}else{
+  			strncpy(databas,argv[ANTARG],15);
+  		}
+  	}
+  }
+/*  fprintf(stderr,"ANTARG=%d,argv[ANTARG]=%s\n",ANTARG,argv[ANTARG]);	*/
+/* Om usr (userid) börjar på 'test' eller 'prov' använd databas 'olfixtst' */
+  if (strncmp(usr,"test",4)==0 || strncmp(usr,"prov",4)==0 ) {
+  	strncpy(databas,"olfixtst",15);
+  }
+ /* fprintf(stderr,"Databas=%s\n",databas);	*/
+/* ================================================================================ */
+/* 		Val av databas, END!						    */
+/* ================================================================================ */
+
+  strncpy(arid,argv[1],strlen(argv[1]));
+
+  strncat(temp5,temp1,strlen(temp1));
 /* SELECT ARID FROM BOKFAR WHERE ARID = "  */
-  strcat(temp5,arid);/* AC */
+  strncat(temp5,arid,strlen(arid));/* AC */
 /* SELECT ARID FROM BOKFAR WHERE ARID = "AC  */
-  strcat(temp5,temp2); /*  "     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
 /* SELECT ARID FROM BOKFAR WHERE ARID = "AC" */
-//  fprintf(stderr,"BARCHKmain: temp5 = %s\n",temp5);
+/*  fprintf(stderr,"BARCHKmain: temp5 = %s\n",temp5);	*/
 
   mysql_init(&my_connection);
-  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", "olfix", 0, NULL, 0)){
+  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", databas, 0, NULL, 0)){
 //  fprintf(stdout,"BARCHKmain:Connection success\n");
   	res = mysql_query(&my_connection,temp5);
   	if (res){
-		printf("Error: BARCHK_SELECT error: %s\n",mysql_error(&my_connection));
+		fprintf(stderr,"Error: BARCHK SELECT error: %s\n",mysql_error(&my_connection));
   	}
   	else{
 		res_ptr=mysql_store_result(&my_connection);
 		if (res_ptr){
-//			fprintf(stderr,"BARCHK_Retrieved %lu rows\n",(unsigned long)mysql_num_rows(res_ptr));
+//			fprintf(stderr,"BARCHK Retrieved %lu rows\n",(unsigned long)mysql_num_rows(res_ptr));
 			if((unsigned long)mysql_num_rows(res_ptr) > 0){ /* En rad har hittats */
 				status=0;
 			}else{						/* Ingen rad hittades */
@@ -82,7 +125,7 @@ int main(int argc, char *argv[])
 			}
 
 			if (mysql_errno(&my_connection)){
-				fprintf(stderr,"Error: BARCHK_Retriev error:  %s\n", mysql_error(&my_connection));
+				fprintf(stderr,"Error: BARCHK Retriev error:  %s\n", mysql_error(&my_connection));
 			}
 		}
 		mysql_free_result(res_ptr);
@@ -90,14 +133,14 @@ int main(int argc, char *argv[])
   	mysql_close(&my_connection);
  }
   else {
-  	fprintf(stderr,"Error: BARCHK_Connection failed\n");
+  	fprintf(stderr,"Error: BARCHK Connection failed\n");
 	if (mysql_errno(&my_connection)){
-		fprintf(stderr,"Error: BARCHK_Connection error %d:  %s\n",
+		fprintf(stderr,"Error: BARCHK Connection error %d:  %s\n",
 		mysql_errno(&my_connection), mysql_error(&my_connection));
 	}
   }
 if(status == 0){
-	fprintf(stdout,"OK: BARCHK_Status = %d\n",status);
+	fprintf(stdout,"OK: BARCHK Status = %d\n",status);
 }
 if(status != 0){
 	fprintf(stderr,"Error: BARCHK Bokföringsår %s finns inte!\n",arid);
@@ -105,3 +148,64 @@ if(status != 0){
   return status;
 }
 
+int which_database(char *envp[])
+{
+	FILE *fil_pek;
+
+	char home[50];
+	char *home_pek;
+	char resource[]="/.olfixrc";
+	char filename[50]="";
+	char tmp[20]="";
+	char temp[10]="";
+	char *tmp_pek;
+	int i,status;
+
+	for (i = 0;envp[i]!=NULL;i++){
+		if(strstr(envp[i],"HOME=") != NULL){
+			strncpy(temp,envp[i],4);
+/*			fprintf(stderr,"temp=%s\n",temp); */
+			status=strcmp(temp,"HOME");
+/*			fprintf(stderr,"status=%d\n",status); */
+			if (status == 0){
+				home_pek=(strstr(envp[i],"HOME="));
+				home_pek=home_pek+5;
+				strcpy(home,home_pek);
+			}
+/*			fprintf(stderr,"home_pek=%d %s\n",home_pek,home_pek);	*/
+		}
+	}
+/*	fprintf(stderr,"home=%s\n",home);	*/
+	strncpy(filename,home,strlen(home));
+	strncat(filename,resource,strlen(resource));
+
+/*	fprintf(stderr,"filename=%s\n",filename);	*/
+	status=-1;
+
+	if ((fil_pek = fopen(filename,"r")) != NULL){
+		while (fgets(tmp,150,fil_pek) != NULL){
+/*			fprintf(stderr,"tmp=%s\n",tmp); */
+			if(strstr(tmp,"DATABASE=")){
+				tmp_pek=(strstr(tmp,"DATABASE="))+9;
+				strncpy(database,tmp_pek,strlen(tmp_pek));
+				status=0;
+			}
+		}
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
+		fclose(fil_pek);
+	}
+	else{
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
+	 	fprintf(stderr,"Error: Filen .olfixrc kan inte öppnas\n");
+	}
+	for (i=0;i < strlen(database);i++){
+		tmp[i]=database[i];
+	}
+	tmp[i-1]=0;
+/*	fprintf(stderr,"tmp=%s, i=%d len(tmp)=%d\n",tmp,i,strlen(tmp));	*/
+	strncpy(database,tmp,strlen(tmp));
+	database[strlen(tmp)]=0;
+/*	fprintf(stderr,"databas=%s\n",database);	*/
+
+	return status;
+}
