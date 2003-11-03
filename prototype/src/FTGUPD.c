01a5aa83294c63ab5f9bc3299aff615c5ccfc1a7
janpihlgren <janpihlgren>
2003-11-03 13:25:02 +0000

    Lagt in val av företag samt kodstädning

--- prototype/src/FTGUPD.c
+++ prototype/src/FTGUPD.c
@@ -1,7 +1,9 @@
 /***************************************************************************
                           FTGUPD.c  -  description
                              -------------------
-    begin                : mån 18 nov
+    Version		 : 0.2
+    begin                : mån 18 nov	2002
+    modified		 : Mån  3 nov 2003
     copyright            : (C) 2002 by Jan Pihlgren
     email                : jan@pihlgren.se
  ***************************************************************************/
@@ -27,7 +29,7 @@
 
 */
  /*@unused@*/ static char RCS_id[] =
-    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/FTGUPD.c,v 1.1 2003/05/08 08:54:07 frazze Exp $ " ;
+    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/FTGUPD.c,v 1.2 2003/11/03 13:25:02 janpihlgren Exp $ " ;
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -38,59 +40,101 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
-
 #include "mysql.h"
 
 #define FORMAT_STR "%Y-%m-%d_%H:%M:%S"    /* YYYY-MM-DD */
 #define MAXSIZE 20
 #define MAXSTRING 5000
 #define TEXTSIZE 255
+#define ANTARG 3
 
-
-int main(int argc, char *argv[])
-{
   MYSQL my_connection;
+  MYSQL_RES *res_ptr;
+  MYSQL_ROW sqlrow;
+
+int which_database(char *envp[]);
+char database[15]="";
+
+int main(int argc, char *argv[], char *envp[])
+{
   int res;
+  int status;
+  const char *userp = getenv("USER");	/* vem är inloggad?	*/
+  char databas[25]="olfix";
+  char usr[15]="";			/* userid		*/
 
   char temp1a[]="UPDATE FTGDATA SET FDATA = ";
   char temp2[]=" WHERE POSTTYP = ";
-  char temp3[]=",";
-  char temp4[]=")";
   char temp5[TEXTSIZE+200]="";
 
-  char ptyp[5];		/* posttyp */
-  char txt[61];		/* ftgdata */
-  int status=-15;
+  char ptyp[5]="";		/* posttyp */
+  char txt[61]="";		/* ftgdata */
 
-   strcpy(ptyp,argv[1]);
-   strcpy(txt,argv[2]);
+  status=-15;
+/* ================================================================================ */
+/* 		Val av databas, START						    */
+/* ================================================================================ */
 
-//    fprintf(stderr,"FTGUPDmain1: argv1 = %s argv2 = %s len text=%d\n",ptyp,txt,strlen(txt));
-//    fprintf(stderr,"FTGUPDmain: argv2 = %s\n",txt);
+  status = which_database(envp);
 
-  strcpy(temp5,temp1a);
+  if (status != 0)
+	exit(status);
+
+  strncpy(usr,userp,15);			/* Den inloggades userid	*/
+/*  fprintf(stderr,"status=%d ANTARG=%d len(database)=%d\n",status,ANTARG,strlen(database));	*/
+  if (argc < ANTARG+1){
+    	if (strlen(database)!= 0){
+		strncpy(databas,database,15);
+	}else{
+  		strncpy(databas,"olfixtst",15);	/* olfixtst = testföretag	*/
+	}
+  }else{
+	if (strlen(argv[ANTARG]) != 0){
+  		if (strncmp(argv[ANTARG],"99",2)==0){
+			strncpy(databas,"olfixtst",15);
+		}else{
+  			strncpy(databas,argv[ANTARG],15);
+  		}
+  	}
+  }
+/*  fprintf(stderr,"ANTARG=%d,argv[ANTARG]=%s\n",ANTARG,argv[ANTARG]);	*/
+/* Om usr (userid) börjar på 'test' eller 'prov' använd databas 'olfixtst' */
+  if (strncmp(usr,"test",4)==0 || strncmp(usr,"prov",4)==0 ) {
+  	strncpy(databas,"olfixtst",15);
+  }
+/* fprintf(stderr,"Databas=%s\n",databas);	*/
+/* ================================================================================ */
+/* 		Val av databas, END!						    */
+/* ================================================================================ */
+
+   strncpy(ptyp,argv[1],strlen(argv[1]));
+   strncpy(txt,argv[2],strlen(argv[2]));
+
+/*    fprintf(stderr,"FTGUPDmain1: argv1 = %s argv2 = %s len text=%d\n",ptyp,txt,strlen(txt));	*/
+/*    fprintf(stderr,"FTGUPDmain: argv2 = %s\n",txt);						*/
+
+  strncpy(temp5,temp1a,strlen(temp1a));
 /* UPDATE FTGDATA SET FDATA =  */
-  strcat(temp5,"'");
-  strcat(temp5,txt);
-  strcat(temp5,"'");
-//  fprintf(stderr,"FTGUPDmain2: temp5=%s\n",temp5);
+  strncat(temp5,"'",1);
+  strncat(temp5,txt,strlen(txt));
+  strncat(temp5,"'",1);
+/*  fprintf(stderr,"FTGUPDmain2: temp5=%s\n",temp5);	*/
 /* UPDATE FTGDATA SET FDATA = 'txt' */
-  strcat(temp5,temp2); /* WHERE POSTTYP =  */
+  strncat(temp5,temp2,strlen(temp2)); /* WHERE POSTTYP =  */
 /* UPDATE FTGDATA SET FDATA = 'txt, WHERE POSTTYP =    */
-  strcat(temp5,"'");
-//  fprintf(stderr,"FTGUPDmain3: temp5=%s\n",temp5);
-  strcat(temp5,ptyp);
-//  fprintf(stderr,"FTGUPDmain4: temp5=%s\n",temp5);
+  strncat(temp5,"'",1);
+/*  fprintf(stderr,"FTGUPDmain3: temp5=%s\n",temp5);	*/
+  strncat(temp5,ptyp,strlen(ptyp));
+/*  fprintf(stderr,"FTGUPDmain4: temp5=%s\n",temp5);	*/
 /* UPDATE FTGDATA SET FDATA = 'txt' WHERE POSTTYP = 'ADR1' */
-  strcat(temp5,"'");
+  strncat(temp5,"'",1);
 
-//fprintf(stderr,"FTGUPDmain: temp5=%s\n",temp5);
+/* fprintf(stderr,"FTGUPDmain: temp5=%s\n",temp5);	*/
 
   mysql_init(&my_connection);
 
-  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", "olfix", 0, NULL, 0)){
-//    fprintf(stderr,"FTGUPD:Connection success\n");
+  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", databas, 0, NULL, 0)){
+/*    fprintf(stderr,"FTGUPD:Connection success\n");	*/
     res = mysql_query(&my_connection,temp5);
   	if (!res){
 		fprintf(stderr,"OK: FTGUPD Inserted %lu rows\n",
@@ -111,4 +155,65 @@ int main(int argc, char *argv[])
   return EXIT_SUCCESS;
 }
 
+int which_database(char *envp[])
+{
+	FILE *fil_pek;
+
+	char home[50];
+	char *home_pek;
+	char resource[]="/.olfixrc";
+	char filename[50]="";
+	char tmp[20]="";
+	char temp[10]="";
+	char *tmp_pek;
+	int i,status;
+
+	for (i = 0;envp[i]!=NULL;i++){
+		if(strstr(envp[i],"HOME=") != NULL){
+			strncpy(temp,envp[i],4);
+/*			fprintf(stderr,"temp=%s\n",temp); */
+			status=strcmp(temp,"HOME");
+/*			fprintf(stderr,"status=%d\n",status); */
+			if (status == 0){
+				home_pek=(strstr(envp[i],"HOME="));
+				home_pek=home_pek+5;
+				strcpy(home,home_pek);
+			}
+/*			fprintf(stderr,"home_pek=%d %s\n",home_pek,home_pek);	*/
+		}
+	}
+/*	fprintf(stderr,"home=%s\n",home);	*/
+	strncpy(filename,home,strlen(home));
+	strncat(filename,resource,strlen(resource));
+
+/*	fprintf(stderr,"filename=%s\n",filename);	*/
+	status=-1;
+
+	if ((fil_pek = fopen(filename,"r")) != NULL){
+		while (fgets(tmp,150,fil_pek) != NULL){
+/*			fprintf(stderr,"tmp=%s\n",tmp); */
+			if(strstr(tmp,"DATABASE=")){
+				tmp_pek=(strstr(tmp,"DATABASE="))+9;
+				strncpy(database,tmp_pek,strlen(tmp_pek));
+				status=0;
+			}
+		}
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
+		fclose(fil_pek);
+	}
+	else{
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
+	 	fprintf(stderr,"Error: Filen .olfixrc kan inte öppnas\n");
+	}
+	for (i=0;i < strlen(database);i++){
+		tmp[i]=database[i];
+	}
+	tmp[i-1]=0;
+/*	fprintf(stderr,"tmp=%s, i=%d len(tmp)=%d\n",tmp,i,strlen(tmp));	*/
+	strncpy(database,tmp,strlen(tmp));
+	database[strlen(tmp)]=0;
+/*	fprintf(stderr,"databas=%s\n",database);	*/
+
+	return status;
+}
 
