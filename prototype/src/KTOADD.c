33ba5a8e6ca25b93ca5b5b3f0072aebb75610c1f
janpihlgren <janpihlgren>
2003-11-04 16:43:41 +0000

    Lagt in val av företag samt kodstädning

--- prototype/src/KTOADD.c
+++ prototype/src/KTOADD.c
@@ -1,8 +1,9 @@
 /***************************************************************************
                           KTOADD.c  -  description
                              -------------------
-    Version		 : 0.01
-    begin                : Tors 16 jan
+    Version		 : 0.2
+    begin                : Tors 16 jan 2003
+    modified		 : Tis   4 nov 2003
     copyright            : (C) 2003 by Jan Pihlgren
     email                : jan@pihlgren.se
  ***************************************************************************/
@@ -26,7 +27,7 @@
 
 */
  /*@unused@*/ static char RCS_id[] =
-    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/KTOADD.c,v 1.1 2003/05/08 08:54:07 frazze Exp $ " ;
+    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/KTOADD.c,v 1.2 2003/11/04 16:43:41 janpihlgren Exp $ " ;
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -34,18 +35,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "mysql.h"
+#define ANTARG 13
 
   MYSQL my_connection;
   MYSQL_RES *res_ptr;
   MYSQL_ROW sqlrow;
 
-
+int which_database(char *envp[]);
 void display_row();
 
-int main(int argc, char *argv[])
+char database[15]="";
+
+int main(int argc, char *argv[], char *envp[])
 {
-  int res,i;
+/*  int i;	*/
+  int res;
+  int status;
+  const char *userp = getenv("USER");	/* vem är inloggad?	*/
+  char databas[25]="olfix";
+  char usr[15];				/* userid		*/
 
 
   char temp1a[]="INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,UB) VALUES (";
@@ -53,119 +63,155 @@ int main(int argc, char *argv[])
   char temp3[]=",";
   char temp4[]=")";
   char temp5[200]="";
-  char arid[5];
-  char ktonr[5];
-  char benamn[101];
-  char manuell[2];
-  char momskod[5];
-  char srunr[4];
-  char kstalle[5];
-  char projekt[5];
-  char subkto[5];
-  char ktoplan[16];
+  char arid[3]="";
+  char ktonr[5]="";
+  char benamn[101]="";
+  char manuell[2]="";
+  char momskod[5]="";
+  char srunr[4]="";
+  char kstalle[5]="";
+  char projekt[5]="";
+  char subkto[5]="";
+  char ktoplan[16]="";
   char ib[]="0.00";
   char ub[]="0.00";
 
+  /* ================================================================================ */
+/* 		Val av databas, START						    */
+/* ================================================================================ */
+
+  status = which_database(envp);
+
+  if (status != 0)
+	exit(status);
+
+  strncpy(usr,userp,15);			/* Den inloggades userid	*/
+/*  fprintf(stderr,"status=%d ANTARG=%d len(database)=%d\n",status,ANTARG,strlen(database));	*/
+  if (argc < ANTARG+1){
+    	if (strlen(database)!= 0){
+		strncpy(databas,database,15);
+	}else{
+  		strncpy(databas,"olfixtst",15);	/* olfixtst = testföretag	*/
+	}
+  }else{
+	if (strlen(argv[ANTARG]) != 0){
+  		if (strncmp(argv[ANTARG],"99",2)==0){
+			strncpy(databas,"olfixtst",15);
+		}else{
+  			strncpy(databas,argv[ANTARG],15);
+  		}
+  	}
+  }
+/*  fprintf(stderr,"ANTARG=%d,argv[ANTARG]=%s\n",ANTARG,argv[ANTARG]);	*/
+/* Om usr (userid) börjar på 'test' eller 'prov' använd databas 'olfixtst' */
+  if (strncmp(usr,"test",4)==0 || strncmp(usr,"prov",4)==0 ) {
+  	strncpy(databas,"olfixtst",15);
+  }
+/* fprintf(stderr,"Databas=%s\n",databas);	*/
+/* ================================================================================ */
+/* 		Val av databas, END!						    */
+/* ================================================================================ */
+
 
   if (argv[1] != NULL){
-  	strcpy(arid,argv[1]);
+  	strncpy(arid,argv[1],strlen(argv[1]));
   }
   else{
   	fprintf(stderr,"Error: KTOADD: Ange årtal!\n");
 	exit(-1);
   }
-  strcpy(ktonr,argv[2]);
-  strcpy(benamn,argv[3]);
-  strcpy(manuell,argv[4]);
-  strcpy(momskod,argv[5]);
-  strcpy(srunr,argv[6]);
-  strcpy(kstalle,argv[7]);
-  strcpy(projekt,argv[8]);
-  strcpy(subkto,argv[9]);
-  strcpy(ktoplan,argv[10]);
+  strncpy(ktonr,argv[2],strlen(argv[2]));
+  strncpy(benamn,argv[3],strlen(argv[3]));
+  strncpy(manuell,argv[4],strlen(argv[4]));
+  strncpy(momskod,argv[5],strlen(argv[5]));
+  strncpy(srunr,argv[6],strlen(argv[6]));
+  strncpy(kstalle,argv[7],strlen(argv[7]));
+  strncpy(projekt,argv[8],strlen(argv[8]));
+  strncpy(subkto,argv[9],strlen(argv[9]));
+  strncpy(ktoplan,argv[10],strlen(argv[10]));
 
-  strcpy(temp5,temp1a);
+  strncpy(temp5,temp1a,strlen(temp1a));
 /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES (   */
-  strcat(temp5,temp2);			/* 2002 */
+  strncat(temp5,temp2,strlen(temp2));			/* 2002 */
 /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES ("  */
-  strcat(temp5,arid);
+  strncat(temp5,arid,strlen(arid));
 /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES ("AC   */
-  strcat(temp5,temp2);
+  strncat(temp5,temp2,strlen(temp2));
 /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES ("AC"   */
-  strcat(temp5,temp3);
+  strncat(temp5,temp3,strlen(temp3));
 /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES ("AC",   */
-  strcat(temp5,temp2);
+  strncat(temp5,temp2,strlen(temp2));
 /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES ("AC"," */
-  strcat(temp5,ktonr);
-  strcat(temp5,temp2);
-  strcat(temp5,temp3);
-  strcat(temp5,temp2);
+  strncat(temp5,ktonr,strlen(ktonr));
+  strncat(temp5,temp2,strlen(temp2));
+  strncat(temp5,temp3,strlen(temp3));
+  strncat(temp5,temp2,strlen(temp2));
 /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES ("AC","1200"," */
-  strcat(temp5,benamn);
-  strcat(temp5,temp2);
-  strcat(temp5,temp3);
-  strcat(temp5,temp2);
+  strncat(temp5,benamn,strlen(benamn));
+  strncat(temp5,temp2,strlen(temp2));
+  strncat(temp5,temp3,strlen(temp3));
+  strncat(temp5,temp2,strlen(temp2));
 /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES ("AC","1200",
 "Testkonto"," */
-  strcat(temp5,manuell);
-  strcat(temp5,temp2);
-  strcat(temp5,temp3);
-  strcat(temp5,temp2);
+  strncat(temp5,manuell,strlen(manuell));
+  strncat(temp5,temp2,strlen(temp2));
+  strncat(temp5,temp3,strlen(temp3));
+  strncat(temp5,temp2,strlen(temp2));
 /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES ("AC","1200",
 "Testkonto","J"," */
-  strcat(temp5,momskod);
-  strcat(temp5,temp2);
-  strcat(temp5,temp3);
-  strcat(temp5,temp2);
+  strncat(temp5,momskod,strlen(momskod));
+  strncat(temp5,temp2,strlen(temp2));
+  strncat(temp5,temp3,strlen(temp3));
+  strncat(temp5,temp2,strlen(temp2));
 /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES ("AC","1200",
 "Testkonto","J","1"," */
-  strcat(temp5,srunr);
-  strcat(temp5,temp2);
-  strcat(temp5,temp3);
-  strcat(temp5,temp2);
+  strncat(temp5,srunr,strlen(srunr));
+  strncat(temp5,temp2,strlen(temp2));
+  strncat(temp5,temp3,strlen(temp3));
+  strncat(temp5,temp2,strlen(temp2));
 /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES ("AC","1200",
 "Testkonto","J","1","100"," */
-  strcat(temp5,kstalle);
-  strcat(temp5,temp2);
-  strcat(temp5,temp3);
-  strcat(temp5,temp2);
+  strncat(temp5,kstalle,strlen(kstalle));
+  strncat(temp5,temp2,strlen(temp2));
+  strncat(temp5,temp3,strlen(temp3));
+  strncat(temp5,temp2,strlen(temp2));
 /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES ("AC","1200",
 "Testkonto","J","1","100","2000"," */
-  strcat(temp5,projekt);
-  strcat(temp5,temp2);
-  strcat(temp5,temp3);
-  strcat(temp5,temp2);
+  strncat(temp5,projekt,strlen(projekt));
+  strncat(temp5,temp2,strlen(temp2));
+  strncat(temp5,temp3,strlen(temp3));
+  strncat(temp5,temp2,strlen(temp2));
 /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES ("AC","1200",
 "Testkonto","J","1","100","2000","3000"," */
-  strcat(temp5,subkto);
-  strcat(temp5,temp2);
-  strcat(temp5,temp3);
-  strcat(temp5,temp2);
+  strncat(temp5,subkto,strlen(subkto));
+  strncat(temp5,temp2,strlen(temp2));
+  strncat(temp5,temp3,strlen(temp3));
+  strncat(temp5,temp2,strlen(temp2));
 /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES ("AC","1200",
 "Testkonto","J","1","100","2000","3000","4000"," */
-  strcat(temp5,ktoplan);
-  strcat(temp5,temp2);
-  strcat(temp5,temp3);
-  strcat(temp5,temp2);
+  strncat(temp5,ktoplan,strlen(ktoplan));
+  strncat(temp5,temp2,strlen(temp2));
+  strncat(temp5,temp3,strlen(temp3));
+  strncat(temp5,temp2,strlen(temp2));
 /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES ("AC","1200",
 "Testkonto","J","1","100","2000","3000","4000","EUBAS97"," */
-  strcat(temp5,ib);
-  strcat(temp5,temp2);
-  strcat(temp5,temp3);
-  strcat(temp5,temp2);
+  strncat(temp5,ib,strlen(ib));
+  strncat(temp5,temp2,strlen(temp2));
+  strncat(temp5,temp3,strlen(temp3));
+  strncat(temp5,temp2,strlen(temp2));
 /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES ("AC","1200",
 "Testkonto","J","1","100","2000","3000","4000","EUBAS97","0.00"," */
-  strcat(temp5,ub);
-  strcat(temp5,temp2);
-  strcat(temp5,temp4);
+  strncat(temp5,ub,strlen(ub));
+  strncat(temp5,temp2,strlen(temp2));
+  strncat(temp5,temp4,strlen(temp4));
   /* INSERT INTO KTOPLAN(ARID,KTONR,BENAMNING,MANUELL,MOMSKOD,SRUNR,KSTALLE,PROJEKT,SUBKTO,KTOPLAN,IB,IB) VALUES ("AC","1200",
 "Testkonto","J","1","100","2000","3000","4000","EUBAS97","0.00","0.00") */
 
-//  fprintf(stderr,"KTOADDmain: temp5 = %s\n",temp5);
+/*  fprintf(stderr,"KTOADDmain: temp5 = %s\n",temp5);	*/
 
   mysql_init(&my_connection);
-  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", "olfix", 0, NULL, 0)){
-//  	fprintf(stderr,"KTOVIEW:Connection success\n");
+  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", databas, 0, NULL, 0)){
+/*  	fprintf(stderr,"KTOVIEW:Connection success\n");		*/
 
   res = mysql_query(&my_connection,temp5);
 
@@ -188,3 +234,64 @@ int main(int argc, char *argv[])
   return EXIT_SUCCESS;
 }
 
+int which_database(char *envp[])
+{
+	FILE *fil_pek;
+
+	char home[50];
+	char *home_pek;
+	char resource[]="/.olfixrc";
+	char filename[50]="";
+	char tmp[20]="";
+	char temp[10]="";
+	char *tmp_pek;
+	int i,status;
+
+	for (i = 0;envp[i]!=NULL;i++){
+		if(strstr(envp[i],"HOME=") != NULL){
+			strncpy(temp,envp[i],4);
+/*			fprintf(stderr,"temp=%s\n",temp); */
+			status=strcmp(temp,"HOME");
+/*			fprintf(stderr,"status=%d\n",status); */
+			if (status == 0){
+				home_pek=(strstr(envp[i],"HOME="));
+				home_pek=home_pek+5;
+				strcpy(home,home_pek);
+			}
+/*			fprintf(stderr,"home_pek=%d %s\n",home_pek,home_pek);	*/
+		}
+	}
+/*	fprintf(stderr,"home=%s\n",home);	*/
+	strncpy(filename,home,strlen(home));
+	strncat(filename,resource,strlen(resource));
+
+/*	fprintf(stderr,"filename=%s\n",filename);	*/
+	status=-1;
+
+	if ((fil_pek = fopen(filename,"r")) != NULL){
+		while (fgets(tmp,150,fil_pek) != NULL){
+/*			fprintf(stderr,"tmp=%s\n",tmp); */
+			if(strstr(tmp,"DATABASE=")){
+				tmp_pek=(strstr(tmp,"DATABASE="))+9;
+				strncpy(database,tmp_pek,strlen(tmp_pek));
+				status=0;
+			}
+		}
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
+		fclose(fil_pek);
+	}
+	else{
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
+	 	fprintf(stderr,"Error: Filen .olfixrc kan inte öppnas\n");
+	}
+	for (i=0;i < strlen(database);i++){
+		tmp[i]=database[i];
+	}
+	tmp[i-1]=0;
+/*	fprintf(stderr,"tmp=%s, i=%d len(tmp)=%d\n",tmp,i,strlen(tmp));	*/
+	strncpy(database,tmp,strlen(tmp));
+	database[strlen(tmp)]=0;
+/*	fprintf(stderr,"databas=%s\n",database);	*/
+
+	return status;
+}
