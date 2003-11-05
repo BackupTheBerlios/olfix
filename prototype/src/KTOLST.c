2d00378e9c10c378f0eb461a82aabc13e143de59
janpihlgren <janpihlgren>
2003-11-05 06:35:54 +0000

    Lagt in val av företag samt kodstädning

--- prototype/src/KTOLST.c
+++ prototype/src/KTOLST.c
@@ -1,8 +1,9 @@
 /***************************************************************************
                           KTOLST.c  -  description
                              -------------------
+    Version		 : 0.3
     begin                : tis 29 okt 2002
-    Version		 : 0.2		sön 23 mars 2003
+    modified		 : Ons  5 nov 2003
     copyright            : (C) 2002 by Jan Pihlgren
     email                : jan@pihlgren.se
  ***************************************************************************/
@@ -25,7 +26,7 @@
 
 */
  /*@unused@*/ static char RCS_id[] =
-    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/KTOLST.c,v 1.1 2003/05/08 08:54:08 frazze Exp $ " ;
+    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/KTOLST.c,v 1.2 2003/11/05 06:37:36 janpihlgren Exp $ " ;
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -33,8 +34,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include "mysql.h"
+#define ANTARG 1
 
   MYSQL my_connection;
   MYSQL_RES *res_ptr;
@@ -42,15 +44,59 @@
 
 
 void display_row();
+int which_database(char *envp[]);
 
-int main(int argc, char *argv[])
+char database[15]="";
+
+int main(int argc, char *argv[], char *envp[])
 {
-  int res,i;
+  int res;
+  int i;
+  int status;
+  const char *userp = getenv("USER");	/* vem är inloggad?	*/
+  char databas[25]="olfix";
+  char usr[15];				/* userid		*/
+
+/* ================================================================================ */
+/* 		Val av databas, START						    */
+/* ================================================================================ */
+
+  status = which_database(envp);
+
+  if (status != 0)
+	exit(status);
+
+  strncpy(usr,userp,15);			/* Den inloggades userid	*/
+/*  fprintf(stderr,"status=%d ANTARG=%d len(database)=%d\n",status,ANTARG,strlen(database));	*/
+  if (argc < ANTARG+1){
+    	if (strlen(database)!= 0){
+		strncpy(databas,database,15);
+	}else{
+  		strncpy(databas,"olfixtst",15);	/* olfixtst = testföretag	*/
+	}
+  }else{
+	if (strlen(argv[ANTARG]) != 0){
+  		if (strncmp(argv[ANTARG],"99",2)==0){
+			strncpy(databas,"olfixtst",15);
+		}else{
+  			strncpy(databas,argv[ANTARG],15);
+  		}
+  	}
+  }
+/*  fprintf(stderr,"ANTARG=%d,argv[ANTARG]=%s\n",ANTARG,argv[ANTARG]);	*/
+/* Om usr (userid) börjar på 'test' eller 'prov' använd databas 'olfixtst' */
+  if (strncmp(usr,"test",4)==0 || strncmp(usr,"prov",4)==0 ) {
+  	strncpy(databas,"olfixtst",15);
+  }
+/* fprintf(stderr,"Databas=%s\n",databas);	*/
+/* ================================================================================ */
+/* 		Val av databas, END!						    */
+/* ================================================================================ */
 
   mysql_init(&my_connection);
 
-  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", "olfix", 0, NULL, 0)){
-//  	fprintf(stderr,"KTOLST:Connection success\n");
+  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", databas, 0, NULL, 0)){
+/*  	fprintf(stderr,"KTOLST:Connection success\n");		*/
 
   res = mysql_query(&my_connection,"SELECT * FROM KTOPLAN ORDER BY ARID, KTONR");
 
@@ -60,10 +106,10 @@ int main(int argc, char *argv[])
 	res_ptr=mysql_store_result(&my_connection);
 	if (res_ptr){
 		i=1;
-//		fprintf(stdout,"KTOLST:Retrieved %lu rows\n",(unsigned long)mysql_num_rows(res_ptr));
+/*		fprintf(stdout,"KTOLST:Retrieved %lu rows\n",(unsigned long)mysql_num_rows(res_ptr));	*/
 		fprintf(stdout,"OK: NR_%lu_:",(unsigned long)mysql_num_rows(res_ptr));
 		while ((sqlrow=mysql_fetch_row(res_ptr)))  {
-		 	//printf("KTOLST:Fetched data....}");
+/*		printf("KTOLST:Fetched data....}");	*/
 			display_row();
 			i++;
 		}
@@ -95,6 +141,67 @@ void display_row()
 		fprintf(stdout,"%s_:",sqlrow[field_count]);
 		field_count++;
 	}
-//	fprintf(stdout,"\n");
+/*	fprintf(stdout,"\n");	*/
 }
 
+int which_database(char *envp[])
+{
+	FILE *fil_pek;
+
+	char home[50];
+	char *home_pek;
+	char resource[]="/.olfixrc";
+	char filename[50]="";
+	char tmp[20]="";
+	char temp[10]="";
+	char *tmp_pek;
+	int i,status;
+
+	for (i = 0;envp[i]!=NULL;i++){
+		if(strstr(envp[i],"HOME=") != NULL){
+			strncpy(temp,envp[i],4);
+/*			fprintf(stderr,"temp=%s\n",temp); */
+			status=strcmp(temp,"HOME");
+/*			fprintf(stderr,"status=%d\n",status); */
+			if (status == 0){
+				home_pek=(strstr(envp[i],"HOME="));
+				home_pek=home_pek+5;
+				strcpy(home,home_pek);
+			}
+/*			fprintf(stderr,"home_pek=%d %s\n",home_pek,home_pek);	*/
+		}
+	}
+/*	fprintf(stderr,"home=%s\n",home);	*/
+	strncpy(filename,home,strlen(home));
+	strncat(filename,resource,strlen(resource));
+
+/*	fprintf(stderr,"filename=%s\n",filename);	*/
+	status=-1;
+
+	if ((fil_pek = fopen(filename,"r")) != NULL){
+		while (fgets(tmp,150,fil_pek) != NULL){
+/*			fprintf(stderr,"tmp=%s\n",tmp); */
+			if(strstr(tmp,"DATABASE=")){
+				tmp_pek=(strstr(tmp,"DATABASE="))+9;
+				strncpy(database,tmp_pek,strlen(tmp_pek));
+				status=0;
+			}
+		}
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
+		fclose(fil_pek);
+	}
+	else{
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
+	 	fprintf(stderr,"Error: Filen .olfixrc kan inte öppnas\n");
+	}
+	for (i=0;i < strlen(database);i++){
+		tmp[i]=database[i];
+	}
+	tmp[i-1]=0;
+/*	fprintf(stderr,"tmp=%s, i=%d len(tmp)=%d\n",tmp,i,strlen(tmp));	*/
+	strncpy(database,tmp,strlen(tmp));
+	database[strlen(tmp)]=0;
+/*	fprintf(stderr,"databas=%s\n",database);	*/
+
+	return status;
+}
--- prototype/src/KTOVIEW.c
+++ prototype/src/KTOVIEW.c
@@ -2,7 +2,8 @@
                           KTOVIEW.c  -  description
                              -------------------
     Version		 : 0.02
-    begin                : Tors 28 nov
+    begin                : Tors 28 nov 2002
+    modified		 : Ons   5 nov 2003
     copyright            : (C) 2002 by Jan Pihlgren
     email                : jan@pihlgren.se
  ***************************************************************************/
@@ -24,7 +25,7 @@
 
 */
  /*@unused@*/ static char RCS_id[] =
-    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/KTOVIEW.c,v 1.1 2003/05/08 08:54:09 frazze Exp $ " ;
+    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/KTOVIEW.c,v 1.2 2003/11/05 06:35:54 janpihlgren Exp $ " ;
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -32,67 +33,111 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "mysql.h"
+#define ANTARG 2
 
   MYSQL my_connection;
   MYSQL_RES *res_ptr;
   MYSQL_ROW sqlrow;
 
-
+int which_database(char *envp[]);
 void display_row();
+char database[15]="";
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char *envp[])
 {
-  int res,i;
+  int i;
+  int res;
+  int status;
+  const char *userp = getenv("USER");	/* vem är inloggad?	*/
+  char databas[25]="olfix";
+  char usr[15];				/* userid		*/
+
 /*	for(i=0;i<=argc;i++){
 		fprintf(stderr,"argv[%d]=%s\n",i,argv[i]);
 	}
 */
+
   char temp1a[]="SELECT KTONR, BENAMNING FROM KTOPLAN WHERE ARID=\"";
   char temp1b[]=" ORDER BY KTONR";
   char temp2[]="\"";
-  char temp3[]=",";
-  char temp4[]=")";
   char temp5[200]="";
-  char arid[5];
-  
+  char arid[3]="";
+
+/* ================================================================================ */
+/* 		Val av databas, START						    */
+/* ================================================================================ */
+
+  status = which_database(envp);
+
+  if (status != 0)
+	exit(status);
+
+  strncpy(usr,userp,15);			/* Den inloggades userid	*/
+/*  fprintf(stderr,"status=%d ANTARG=%d len(database)=%d\n",status,ANTARG,strlen(database));	*/
+  if (argc < ANTARG+1){
+    	if (strlen(database)!= 0){
+		strncpy(databas,database,15);
+	}else{
+  		strncpy(databas,"olfixtst",15);	/* olfixtst = testföretag	*/
+	}
+  }else{
+	if (strlen(argv[ANTARG]) != 0){
+  		if (strncmp(argv[ANTARG],"99",2)==0){
+			strncpy(databas,"olfixtst",15);
+		}else{
+  			strncpy(databas,argv[ANTARG],15);
+  		}
+  	}
+  }
+/*  fprintf(stderr,"ANTARG=%d,argv[ANTARG]=%s\n",ANTARG,argv[ANTARG]);	*/
+/* Om usr (userid) börjar på 'test' eller 'prov' använd databas 'olfixtst' */
+  if (strncmp(usr,"test",4)==0 || strncmp(usr,"prov",4)==0 ) {
+  	strncpy(databas,"olfixtst",15);
+  }
+/* fprintf(stderr,"Databas=%s\n",databas);	*/
+/* ================================================================================ */
+/* 		Val av databas, END!						    */
+/* ================================================================================ */
+
   if (argv[1] != NULL){
-  	strcpy(arid,argv[1]);
+  	strncpy(arid,argv[1],strlen(argv[1]));
   }
   else{
-  	fprintf(stderr,"Error: KTOVIEW_ARID Ange årtal!\n");
+  	fprintf(stderr,"Error: KTOVIEW ARID Ange årtal!\n");
 	exit(-1);
   }
 
-  strcpy(temp5,temp1a);
+  strncpy(temp5,temp1a,strlen(temp1a));
 /* SELECT KTONR, BENAMNING FROM KTOPLAN WHERA ARID="   */
-  strcat(temp5,arid);			/* 2002 */
+  strncat(temp5,arid,strlen(arid));			/* 2002 */
 /* SELECT KTONR, BENAMNING FROM KTOPLAN WHERA ARID="2002  */
-  strcat(temp5,temp2); /*  "     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
 /* SELECT KTONR, BENAMNING FROM KTOPLAN WHERA ARID="2002"   */
-  strcat(temp5,temp1b);
+  strncat(temp5,temp1b,strlen(temp1b));
 /* SELECT KTONR, BENAMNING FROM KTOPLAN WHERA ARID="2002" ORDER BY KTONR   */
 //  fprintf(stderr,"KTOVIEWmain: temp5 = %s\n",temp5);
 
 
 
   mysql_init(&my_connection);
-  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", "olfix", 0, NULL, 0)){
-//  	fprintf(stderr,"KTOVIEW:Connection success\n");
+  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", databas, 0, NULL, 0)){
+/*  	fprintf(stderr,"KTOVIEW:Connection success\n");		*/
 
   res = mysql_query(&my_connection,temp5);
 
   if (res){
-	fprintf(stderr,"Error: KTOVIEW_SELECT error: %s\n",mysql_error(&my_connection));
+	fprintf(stderr,"Error: KTOVIEW SELECT error: %s\n",mysql_error(&my_connection));
         }else{
 	res_ptr=mysql_store_result(&my_connection);
 	if (res_ptr){
 		i=1;
-//		fprintf(stderr,"KTOVIEW:Retrieved %lu rows\n",(unsigned long)mysql_num_rows(res_ptr));
+/*		fprintf(stderr,"KTOVIEW:Retrieved %lu rows\n",(unsigned long)mysql_num_rows(res_ptr));	*/
 		if ( (unsigned long)mysql_num_rows(res_ptr) > 0){
 			fprintf(stdout,"NR_%lu_:",(unsigned long)mysql_num_rows(res_ptr));
 			while ((sqlrow=mysql_fetch_row(res_ptr)))  {
-		 		//printf("KTOVIEW:Fetched data....}");
+/*			fprintf(stderr,"KTOVIEW:Fetched data....}");	*/
 				display_row();
 				i++;
 			}
@@ -102,7 +147,7 @@ int main(int argc, char *argv[])
 		}
 
 	if (mysql_errno(&my_connection))  {
-		fprintf(stderr,"Error: KTOVIEW_Retriev error:  %s\n", mysql_error(&my_connection));
+		fprintf(stderr,"Error: KTOVIEW Retriev error:  %s\n", mysql_error(&my_connection));
 		}
 	}
 	mysql_free_result(res_ptr);
@@ -110,9 +155,9 @@ int main(int argc, char *argv[])
 	mysql_close(&my_connection);
   	}
 	else {
-    		fprintf(stderr,"Error: KTOVIEW_Connection failed\n");
+    		fprintf(stderr,"Error: KTOVIEW Connection failed\n");
     		if (mysql_errno(&my_connection))   {
-    			fprintf(stderr,"Error: KTOVIEW_Connection error %d:  %s\n",
+    			fprintf(stderr,"Error: KTOVIEW Connection error %d:  %s\n",
 				mysql_errno(&my_connection), mysql_error(&my_connection));
 		}
     	}
@@ -131,3 +176,64 @@ void display_row()
 	}
 }
 
+int which_database(char *envp[])
+{
+	FILE *fil_pek;
+
+	char home[50];
+	char *home_pek;
+	char resource[]="/.olfixrc";
+	char filename[50]="";
+	char tmp[20]="";
+	char temp[10]="";
+	char *tmp_pek;
+	int i,status;
+
+	for (i = 0;envp[i]!=NULL;i++){
+		if(strstr(envp[i],"HOME=") != NULL){
+			strncpy(temp,envp[i],4);
+/*			fprintf(stderr,"temp=%s\n",temp); */
+			status=strcmp(temp,"HOME");
+/*			fprintf(stderr,"status=%d\n",status); */
+			if (status == 0){
+				home_pek=(strstr(envp[i],"HOME="));
+				home_pek=home_pek+5;
+				strcpy(home,home_pek);
+			}
+/*			fprintf(stderr,"home_pek=%d %s\n",home_pek,home_pek);	*/
+		}
+	}
+/*	fprintf(stderr,"home=%s\n",home);	*/
+	strncpy(filename,home,strlen(home));
+	strncat(filename,resource,strlen(resource));
+
+/*	fprintf(stderr,"filename=%s\n",filename);	*/
+	status=-1;
+
+	if ((fil_pek = fopen(filename,"r")) != NULL){
+		while (fgets(tmp,150,fil_pek) != NULL){
+/*			fprintf(stderr,"tmp=%s\n",tmp); */
+			if(strstr(tmp,"DATABASE=")){
+				tmp_pek=(strstr(tmp,"DATABASE="))+9;
+				strncpy(database,tmp_pek,strlen(tmp_pek));
+				status=0;
+			}
+		}
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
+		fclose(fil_pek);
+	}
+	else{
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
+	 	fprintf(stderr,"Error: Filen .olfixrc kan inte öppnas\n");
+	}
+	for (i=0;i < strlen(database);i++){
+		tmp[i]=database[i];
+	}
+	tmp[i-1]=0;
+/*	fprintf(stderr,"tmp=%s, i=%d len(tmp)=%d\n",tmp,i,strlen(tmp));	*/
+	strncpy(database,tmp,strlen(tmp));
+	database[strlen(tmp)]=0;
+/*	fprintf(stderr,"databas=%s\n",database);	*/
+
+	return status;
+}
