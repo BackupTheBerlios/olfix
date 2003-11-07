7a17146f38997703d5de6878691c958334550fdc
janpihlgren <janpihlgren>
2003-11-07 14:49:08 +0000

    Lagt in val av företag samt kodstädning

--- prototype/src/PRGLST.c
+++ prototype/src/PRGLST.c
@@ -1,8 +1,9 @@
 /***************************************************************************
                           PRGLST.c  -  description
                              -------------------
-			     Ver: 0.2
+    Version		 : 0.3
     begin                : Tors  3  april	2003
+    modified		 : Fre   7  nov 2003
     copyright            : (C) 2003 by Jan Pihlgren
     email                : jan@pihlgren.se
  ***************************************************************************/
@@ -28,7 +29,7 @@
 
 */
  /*@unused@*/ static char RCS_id[] =
-    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/PRGLST.c,v 1.1 2003/05/08 08:54:09 frazze Exp $ " ;
+    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/PRGLST.c,v 1.2 2003/11/07 14:49:08 janpihlgren Exp $ " ;
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -37,54 +38,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "mysql.h"
+#define ANTARG 1
 
   MYSQL my_connection;
   MYSQL_RES *res_ptr;
   MYSQL_ROW sqlrow;
 
+int which_database(char *envp[]);
 void display_row();
 
-int main(int argc, char *argv[])
+char database[15]="";
+
+int main(int argc, char *argv[], char *envp[])
 {
 
-//  MYSQL my_connection;
-  int res,i;
+  int i;
+  int res;
+  int status;
+  const char *userp = getenv("USER");	/* vem är inloggad?	*/
+  char databas[25]="olfix";
+  char usr[15];				/* userid		*/
 
   char temp1[]="SELECT * FROM PROGRAM ORDER BY MENYAVD,MENYGRP,PROGRAM";
-  char temp2[]="\"";
-  char temp3[]=",";
-  char temp4[]=")";
   char temp5[200]="";
-  char progid[8];
 
-//  strcpy(progid,argv[1]);
+/* ================================================================================ */
+/* 		Val av databas, START						    */
+/* ================================================================================ */
 
-  strcat(temp5,temp1);
+  status = which_database(envp);
+
+  if (status != 0)
+	exit(status);
+
+  strncpy(usr,userp,15);			/* Den inloggades userid	*/
+/*  fprintf(stderr,"status=%d ANTARG=%d len(database)=%d\n",status,ANTARG,strlen(database));	*/
+  if (argc < ANTARG+1){
+    	if (strlen(database)!= 0){
+		strncpy(databas,database,15);
+	}else{
+  		strncpy(databas,"olfixtst",15);	/* olfixtst = testföretag	*/
+	}
+  }else{
+	if (strlen(argv[ANTARG]) != 0){
+  		if (strncmp(argv[ANTARG],"99",2)==0){
+			strncpy(databas,"olfixtst",15);
+		}else{
+  			strncpy(databas,argv[ANTARG],15);
+  		}
+  	}
+  }
+/*  fprintf(stderr,"ANTARG=%d,argv[ANTARG]=%s\n",ANTARG,argv[ANTARG]);	*/
+/* Om usr (userid) börjar på 'test' eller 'prov' använd databas 'olfixtst' */
+  if (strncmp(usr,"test",4)==0 || strncmp(usr,"prov",4)==0 ) {
+  	strncpy(databas,"olfixtst",15);
+  }
+/* fprintf(stderr,"Databas=%s\n",databas);	*/
+/* ================================================================================ */
+/* 		Val av databas, END!						    */
+/* ================================================================================ */
+
+/*  strcpy(progid,argv[1]);	*/
+
+  strncat(temp5,temp1,strlen(temp1));
 /* SELECT * FROM PROGRAM  */
 
   mysql_init(&my_connection);
 
-  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", "olfix", 0, NULL, 0)){
-//  fprintf(stdout,"PGMLST:Connection success\n");
+  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", databas, 0, NULL, 0)){
+/*  fprintf(stdout,"PGMLST:Connection success\n");	*/
 
   res = mysql_query(&my_connection,temp5);
 
   if (res){
-	fprintf(stderr,"Error: PRGLST_SELECT error: %s\n",mysql_error(&my_connection));
+	fprintf(stderr,"Error: PRGLST SELECT error: %s\n",mysql_error(&my_connection));
         }else{
 	res_ptr=mysql_store_result(&my_connection);
 	if (res_ptr){
 		i=1;
 		fprintf(stdout,"NR_%lu_:",(unsigned long)mysql_num_rows(res_ptr));
 		while ((sqlrow=mysql_fetch_row(res_ptr)))  {
-		 	//printf("RGTDSP:Fetched data....}");
+/*		fprintf(stderr,"RGTDSP:Fetched data....}");	*/
 			display_row();
 			i++;
 		}
 	if (mysql_errno(&my_connection))  {
-		fprintf(stderr,"Error: PRGLST_Retriev error:  %s\n", mysql_error(&my_connection));
+		fprintf(stderr,"Error: PRGLST Retriev error:  %s\n", mysql_error(&my_connection));
 		}
 	}
 	mysql_free_result(res_ptr);
@@ -92,9 +132,9 @@ int main(int argc, char *argv[])
 	mysql_close(&my_connection);
 
   	} else {
-    	fprintf(stderr,"Error: PRGLST_Connection failed\n");
+    	fprintf(stderr,"Error: PRGLST Connection failed\n");
     	if (mysql_errno(&my_connection))   {
-    		fprintf(stderr,"Error: PRGLST_Connection error %d:  %s\n",
+    		fprintf(stderr,"Error: PRGLST Connection error %d:  %s\n",
 			mysql_errno(&my_connection), mysql_error(&my_connection));
 	}
     }
@@ -111,5 +151,67 @@ void display_row()
 		fprintf(stdout,"(%d)%s_:",field_count,sqlrow[field_count]);
 		field_count++;
 	}
-//	fprintf(stdout,"\n");
+/*	fprintf(stdout,"\n");	*/
+}
+
+int which_database(char *envp[])
+{
+	FILE *fil_pek;
+
+	char home[50];
+	char *home_pek;
+	char resource[]="/.olfixrc";
+	char filename[50]="";
+	char tmp[20]="";
+	char temp[10]="";
+	char *tmp_pek;
+	int i,status;
+
+	for (i = 0;envp[i]!=NULL;i++){
+		if(strstr(envp[i],"HOME=") != NULL){
+			strncpy(temp,envp[i],4);
+/*			fprintf(stderr,"temp=%s\n",temp); */
+			status=strcmp(temp,"HOME");
+/*			fprintf(stderr,"status=%d\n",status); */
+			if (status == 0){
+				home_pek=(strstr(envp[i],"HOME="));
+				home_pek=home_pek+5;
+				strcpy(home,home_pek);
+			}
+/*			fprintf(stderr,"home_pek=%d %s\n",home_pek,home_pek);	*/
+		}
+	}
+/*	fprintf(stderr,"home=%s\n",home);	*/
+	strncpy(filename,home,strlen(home));
+	strncat(filename,resource,strlen(resource));
+
+/*	fprintf(stderr,"filename=%s\n",filename);	*/
+	status=-1;
+
+	if ((fil_pek = fopen(filename,"r")) != NULL){
+		while (fgets(tmp,150,fil_pek) != NULL){
+/*			fprintf(stderr,"tmp=%s\n",tmp); */
+			if(strstr(tmp,"DATABASE=")){
+				tmp_pek=(strstr(tmp,"DATABASE="))+9;
+				strncpy(database,tmp_pek,strlen(tmp_pek));
+				status=0;
+			}
+		}
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
+		fclose(fil_pek);
+	}
+	else{
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
+	 	fprintf(stderr,"Error: Filen .olfixrc kan inte öppnas\n");
+	}
+	for (i=0;i < strlen(database);i++){
+		tmp[i]=database[i];
+	}
+	tmp[i-1]=0;
+/*	fprintf(stderr,"tmp=%s, i=%d len(tmp)=%d\n",tmp,i,strlen(tmp));	*/
+	strncpy(database,tmp,strlen(tmp));
+	database[strlen(tmp)]=0;
+/*	fprintf(stderr,"databas=%s\n",database);	*/
+
+	return status;
 }
