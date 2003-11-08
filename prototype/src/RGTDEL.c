742e250842fb525b64dd1d216e8bcca629fe835c
janpihlgren <janpihlgren>
2003-11-08 12:04:25 +0000

    Lagt in val av företag samt kodstädning och buggrättning

--- prototype/src/RGTDEL.c
+++ prototype/src/RGTDEL.c
@@ -1,8 +1,9 @@
 /***************************************************************************
                           RGTDEL.c  -  description
                              -------------------
-			     ver 0.003			Felhantering
+    Version		 : 0.4
     begin                : Mån 3 febr	2003
+    modified		 : Lör 8 nov    2003
     copyright            : (C) 2003 by Jan Pihlgren
     email                : jan@pihlgren.se
  ***************************************************************************/
@@ -27,7 +28,7 @@
 
 */
  /*@unused@*/ static char RCS_id[] =
-    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/RGTDEL.c,v 1.1 2003/05/08 08:54:10 frazze Exp $ " ;
+    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/RGTDEL.c,v 1.2 2003/11/08 12:04:25 janpihlgren Exp $ " ;
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -36,73 +37,173 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "mysql.h"
+#define ANTARG 3
 
-
-int main(int argc, char *argv[])
-{
   MYSQL my_connection;
+  MYSQL_RES *res_ptr;
+  MYSQL_ROW sqlrow;
+
+int which_database(char *envp[]);
+
+char database[15]="";
+
+int main(int argc, char *argv[], char *envp[])
+{
+/* int i;	*/
   int res;
-//  int i;
+  int status;
+  const char *userp = getenv("USER");	/* vem är inloggad?	*/
+  char databas[25]="olfix";
+  char usr[15];				/* userid		*/
 
-//  for(i=0;i<argc;i++){
-//  	fprintf(stderr,"argv%d = %s\n",i,argv[i]);
-//  }
-
-
+/*  for(i=0;i<argc;i++){
+  	fprintf(stderr,"argv%d = %s\n",i,argv[i]);
+  }
+*/
 
   char temp1[]="DELETE FROM RIGHTS WHERE USERID = \"";
   char temp2[]="\"";
-  char temp3[]=",";
-  char temp4[]=")";
   char temp5[200]="";
   char temp6[]=" AND TRNSID =\"";
-  char userid[9];
-  char trnsid[9];
+  char userid[9]="";
+  char trnsid[9]="";
 
-  strcpy(userid,argv[1]);
-  strcpy(trnsid,argv[2]);
+/* ================================================================================ */
+/* 		Val av databas, START						    */
+/* ================================================================================ */
 
-  strcpy(temp5,temp1);
+  status = which_database(envp);
+
+  if (status != 0)
+	exit(status);
+
+  strncpy(usr,userp,15);			/* Den inloggades userid	*/
+/*  fprintf(stderr,"status=%d ANTARG=%d len(database)=%d\n",status,ANTARG,strlen(database));	*/
+  if (argc < ANTARG+1){
+    	if (strlen(database)!= 0){
+		strncpy(databas,database,15);
+	}else{
+  		strncpy(databas,"olfixtst",15);	/* olfixtst = testföretag	*/
+	}
+  }else{
+	if (strlen(argv[ANTARG]) != 0){
+  		if (strncmp(argv[ANTARG],"99",2)==0){
+			strncpy(databas,"olfixtst",15);
+		}else{
+  			strncpy(databas,argv[ANTARG],15);
+  		}
+  	}
+  }
+/*  fprintf(stderr,"ANTARG=%d,argv[ANTARG]=%s\n",ANTARG,argv[ANTARG]);	*/
+/* Om usr (userid) börjar på 'test' eller 'prov' använd databas 'olfixtst' */
+  if (strncmp(usr,"test",4)==0 || strncmp(usr,"prov",4)==0 ) {
+  	strncpy(databas,"olfixtst",15);
+  }
+/* fprintf(stderr,"Databas=%s\n",databas);	*/
+/* ================================================================================ */
+/* 		Val av databas, END!						    */
+/* ================================================================================ */
+
+  strncpy(userid,argv[1],strlen(argv[1]));
+  strncpy(trnsid,argv[2],strlen(argv[2]));
+
+  strncpy(temp5,temp1,strlen(temp1));
 /* DELETE FROM RIGHTS WHERE USERID = "  */
-  strcat(temp5,userid);/* KALLE */
+  strncat(temp5,userid,strlen(userid));/* KALLE */
 /* DELETE FROM RIGHTS WHERE USERID = "KALLE  */
-  strcat(temp5,temp2); /*  "     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
 /* DELETE FROM RIGHTS WHERE USERID = "KALLE" */
-  strcat(temp5,temp6);
+  strncat(temp5,temp6,strlen(temp6));
 /* DELETE FROM RIGHTS WHERE USERID = "KALLE" AND TRNSID ="  */
-  strcat(temp5,trnsid); /* USERADD */
+  strncat(temp5,trnsid,strlen(trnsid)); /* USERADD */
 /* DELETE FROM RIGHTS WHERE USERID = "KALLE" AND TRNSID ="USERADD  */
-  strcat(temp5,temp2);
+  strncat(temp5,temp2,strlen(temp2));
 /* DELETE FROM RIGHTS WHERE USERID = "KALLE" AND TRNSID ="USERADD"  */
-//  fprintf(stderr,"SQLkommando= %s\n",temp5);
+/*  fprintf(stderr,"SQLkommando= %s\n",temp5);				*/
 
   mysql_init(&my_connection);
 
-  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", "olfix", 0, NULL, 0)){
-//  fprintf(stdout,"RGTDEL_Connection success\n");
+  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", databas, 0, NULL, 0)){
+/*  fprintf(stdout,"RGTDEL Connection success\n");	*/
 
-    res = mysql_query(&my_connection,temp5);
-
-  	if (!res){
-		fprintf(stdout,"OK: RGTDEL_Deleted %lu rows\n",
-			(unsigned long)mysql_affected_rows(&my_connection));
+    	res = mysql_query(&my_connection,temp5);
+//	fprintf(stderr,"RGTDEL res = %d\n",res);
+	fprintf(stdout,"RGTDEL affeted rows = %lu rows\n",(unsigned long)mysql_affected_rows(&my_connection));
+	if ((unsigned long)mysql_affected_rows(&my_connection) < 1){
+		fprintf(stderr,"Error: RGTDEL DELETE ERROR: Ingen post raderad\n");
         }else{
-	fprintf(stderr,"Error: RGTDEL_INSERT error: %d  %s\n", mysql_errno(&my_connection),
-					mysql_error(&my_connection));
-        }
+		fprintf(stdout,"OK: RGTDEL Deleted %lu rows\n",(unsigned long)mysql_affected_rows(&my_connection));
+	}
 	mysql_close(&my_connection);
-
  } else {
-	fprintf(stderr,"Error: RGTDEL_Connection failed\n");
+	fprintf(stderr,"Error: RGTDEL Connection failed\n");
     	if (mysql_errno(&my_connection))   {
-    		fprintf(stderr,"Error: RGTDEL_Connection error %d:  %s\n",
+    		fprintf(stderr,"Error: RGTDEL Connection error %d:  %s\n",
 			mysql_errno(&my_connection), mysql_error(&my_connection));
 	}
     }
   return EXIT_SUCCESS;
 }
 
+int which_database(char *envp[])
+{
+	FILE *fil_pek;
 
+	char home[50];
+	char *home_pek;
+	char resource[]="/.olfixrc";
+	char filename[50]="";
+	char tmp[20]="";
+	char temp[10]="";
+	char *tmp_pek;
+	int i,status;
 
+	for (i = 0;envp[i]!=NULL;i++){
+		if(strstr(envp[i],"HOME=") != NULL){
+			strncpy(temp,envp[i],4);
+/*			fprintf(stderr,"temp=%s\n",temp); */
+			status=strcmp(temp,"HOME");
+/*			fprintf(stderr,"status=%d\n",status); */
+			if (status == 0){
+				home_pek=(strstr(envp[i],"HOME="));
+				home_pek=home_pek+5;
+				strcpy(home,home_pek);
+			}
+/*			fprintf(stderr,"home_pek=%d %s\n",home_pek,home_pek);	*/
+		}
+	}
+/*	fprintf(stderr,"home=%s\n",home);	*/
+	strncpy(filename,home,strlen(home));
+	strncat(filename,resource,strlen(resource));
+
+/*	fprintf(stderr,"filename=%s\n",filename);	*/
+	status=-1;
+
+	if ((fil_pek = fopen(filename,"r")) != NULL){
+		while (fgets(tmp,150,fil_pek) != NULL){
+/*			fprintf(stderr,"tmp=%s\n",tmp); */
+			if(strstr(tmp,"DATABASE=")){
+				tmp_pek=(strstr(tmp,"DATABASE="))+9;
+				strncpy(database,tmp_pek,strlen(tmp_pek));
+				status=0;
+			}
+		}
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
+		fclose(fil_pek);
+	}
+	else{
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
+	 	fprintf(stderr,"Error: Filen .olfixrc kan inte öppnas\n");
+	}
+	for (i=0;i < strlen(database);i++){
+		tmp[i]=database[i];
+	}
+	tmp[i-1]=0;
+/*	fprintf(stderr,"tmp=%s, i=%d len(tmp)=%d\n",tmp,i,strlen(tmp));	*/
+	strncpy(database,tmp,strlen(tmp));
+	database[strlen(tmp)]=0;
+/*	fprintf(stderr,"databas=%s\n",database);	*/
+
+	return status;
+}
