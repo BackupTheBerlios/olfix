82f2a65f58b5877db5e0e826a2ac55cc7bd77169
janpihlgren <janpihlgren>
2003-11-09 04:43:07 +0000

    Kodstädning

--- prototype/src/SLPADD.c
+++ prototype/src/SLPADD.c
@@ -2,8 +2,8 @@
                           SLPADD.c  -  description
                              -------------------
     Vesrsion		 : 0.2
-    Modifierad		 : Mån 20 okt 2003
     begin                : Ons 10 aug 2003
+    Modifierad		 : Sön  9 nov 2003
     copyright            : (C) 2003 by Jan Pihlgren
     email                : jan@pihlgren.se
  ***************************************************************************/
@@ -28,7 +28,7 @@
 
 */
  /*@unused@*/ static char RCS_id[] =
-    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/SLPADD.c,v 1.2 2003/10/20 06:15:00 janpihlgren Exp $ " ;
+    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/SLPADD.c,v 1.3 2003/11/09 04:43:07 janpihlgren Exp $ " ;
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -37,8 +37,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "mysql.h"
+#define ANTARG 7
 
   MYSQL my_connection;
   MYSQL_RES *res_ptr;
@@ -49,7 +49,9 @@
 
 int main(int argc, char *argv[], char *envp[])
 {
-  int res,status;
+/* int i;	*/
+  int res;
+  int status;
   char databas[25]="olfix";
   const char *userp = getenv("USER");	// vem är inloggad?
   char usr[15];				// userid
@@ -59,94 +61,97 @@ int main(int argc, char *argv[], char *envp[])
   char temp3[]=",";
   char temp4[]=")";
   char temp5[200]="";
-  char kundnr[11];
-  char stdlevplats[4];
-  char adress[31];
-  char postnr[7];
-  char postadr[31];
-  char land[31];
-//  fprintf(stderr,"argc=%d, argv[argc-1]=%s\n",argc,argv[argc-1]);
-// ================================================================================
-// 		Val av databas, START
-// ================================================================================
+  char kundnr[11]="";
+  char stdlevplats[4]="";
+  char adress[31]="";
+  char postnr[7]="";
+  char postadr[31]="";
+  char land[31]="";
+
+/*  fprintf(stderr,"argc=%d, argv[argc-1]=%s\n",argc,argv[argc-1]);	*/
+/* ================================================================================ */
+/* 		Val av databas, START						    */
+/* ================================================================================ */
+
   status = which_database(envp);
 
   if (status != 0)
 	exit(status);
 
-  strcpy(usr,userp);				// Den inloggades userid
-
- if (argc<8){
+  strncpy(usr,userp,15);			/* Den inloggades userid	*/
+/*  fprintf(stderr,"status=%d ANTARG=%d len(database)=%d\n",status,ANTARG,strlen(database));	*/
+  if (argc < ANTARG+1){
     	if (strlen(database)!= 0){
-		strcpy(databas,database);
+		strncpy(databas,database,15);
 	}else{
-  		strcpy(databas,"olfixtst");	// olfixtst = testföretag
+  		strncpy(databas,"olfixtst",15);	/* olfixtst = testföretag	*/
 	}
   }else{
-	if (strlen(argv[7]) != 0){
-  		if (strncmp(argv[7],"99",2)==0){
-			strcpy(databas,"olfixtst");
+	if (strlen(argv[ANTARG]) != 0){
+  		if (strncmp(argv[ANTARG],"99",2)==0){
+			strncpy(databas,"olfixtst",15);
 		}else{
-  			strcpy(databas,argv[7]);
+  			strncpy(databas,argv[ANTARG],15);
   		}
   	}
   }
-  /* Om usr (userid) börjar på 'test' eller 'prov' använd databas 'olfixtst' */
+/*  fprintf(stderr,"ANTARG=%d,argv[ANTARG]=%s\n",ANTARG,argv[ANTARG]);	*/
+/* Om usr (userid) börjar på 'test' eller 'prov' använd databas 'olfixtst' */
   if (strncmp(usr,"test",4)==0 || strncmp(usr,"prov",4)==0 ) {
-  	strcpy(databas,"olfixtst");
+  	strncpy(databas,"olfixtst",15);
   }
-// ================================================================================
-// 		Val av databas, END!
-// ================================================================================
-//  fprintf(stderr,"databas=%s\n",databas);
+/* fprintf(stderr,"Databas=%s\n",databas);	*/
+/* ================================================================================ */
+/* 		Val av databas, END!						    */
+/* ================================================================================ */
 
-  strcpy(kundnr,argv[1]);
-  strcpy(stdlevplats,argv[2]);
-  strcpy(adress,argv[3]);
-  strcpy(postnr,argv[4]);
-  strcpy(postadr,argv[5]);
-  strcpy(land,argv[6]);
+  strncpy(kundnr,argv[1],strlen(argv[1]));
+  strncpy(stdlevplats,argv[2],strlen(argv[2]));
+  strncpy(adress,argv[3],strlen(argv[3]));
+  strncpy(postnr,argv[4],strlen(argv[4]));
+  strncpy(postadr,argv[5],strlen(argv[5]));
+  strncpy(land,argv[6],strlen(argv[6]));
 
 
-  strcat(temp5,temp1);
+  strncpy(temp5,temp1,strlen(temp1));
 /* INSERT INTO STDLEVPLATS(KUNDNR,STDLEVPLATS,ADRESS,POSTNR,POSTADR,LAND) VALUES ("  */
-  strcat(temp5,kundnr);/* 4376 */
+  strncat(temp5,kundnr,strlen(kundnr));/* 4376 */
 /* INSERT INTO STDLEVPLATS(KUNDNR,STDLEVPLATS,ADRESS,POSTNR,POSTADR,LAND) VALUES ("4376  */
-  strcat(temp5,temp2); /*  "     */
-  strcat(temp5,temp3); /*  ,     */
-  strcat(temp5,temp2); /*  "     */
-  strcat(temp5,stdlevplats);/* 001  */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
+  strncat(temp5,temp3,strlen(temp3)); /*  ,     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
+  strncat(temp5,stdlevplats,strlen(stdlevplats));/* 001  */
 /* INSERT INTO STDLEVPLATS(KUNDNR,STDLEVPLATS,ADRESS,POSTNR,POSTADR,LAND) VALUES ("4376","001 */
-  strcat(temp5,temp2); /*  "     */
-  strcat(temp5,temp3); /*  ,     */
-  strcat(temp5,temp2); /*  "     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
+  strncat(temp5,temp3,strlen(temp3)); /*  ,     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
 /* INSERT INTO STDLEVPLATS(KUNDNR,STDLEVPLATS,ADRESS,POSTNR,POSTADR,LAND) VALUES ("4376","001","  */
-  strcat(temp5,adress);
-  strcat(temp5,temp2); /*  "     */
-  strcat(temp5,temp3); /*  ,     */
-  strcat(temp5,temp2); /*  "     */
+  strncat(temp5,adress,strlen(adress));
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
+  strncat(temp5,temp3,strlen(temp3)); /*  ,     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
 /* INSERT INTO STDLEVPLATS(KUNDNR,STDLEVPLATS,ADRESS,POSTNR,POSTADR,LAND) VALUES ("4376","001","Provstigen 1","  */
-  strcat(temp5,postnr);
-  strcat(temp5,temp2); /*  "     */
-  strcat(temp5,temp3); /*  ,     */
-  strcat(temp5,temp2); /*  "     */
+  strncat(temp5,postnr,strlen(postnr));
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
+  strncat(temp5,temp3,strlen(temp3)); /*  ,     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
 /* INSERT INTO STDLEVPLATS(KUNDNR,STDLEVPLATS,ADRESS,POSTNR,POSTADR,LAND) VALUES ("4376","001","Provstigen 1","199 99","  */
-  strcat(temp5,postadr);
-  strcat(temp5,temp2); /*  "     */
-  strcat(temp5,temp3); /*  ,     */
-  strcat(temp5,temp2); /*  "     */
+  strncat(temp5,postadr,strlen(postadr));
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
+  strncat(temp5,temp3,strlen(temp3)); /*  ,     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
 /* INSERT INTO STDLEVPLATS(KUNDNR,STDLEVPLATS,ADRESS,POSTNR,POSTADR,LAND) VALUES ("4376","001","Provstigen 1","199 99","LILLEBY","  */
-  strcat(temp5,land);
-  strcat(temp5,temp2); /*  "     */
-  strcat(temp5,temp4); /*  )     */
+  strncat(temp5,land,strlen(land));
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
+  strncat(temp5,temp4,strlen(temp4)); /*  )     */
 /* INSERT INTO STDLEVPLATS(KUNDNR,STDLEVPLATS,ADRESS,POSTNR,POSTADR,LAND) VALUES ("4376","001","Provstigen 1","199 99","LILLEBY","Svberige")  */
 
- // fprintf(stderr,"temp5=%s\n",temp5);
+/* fprintf(stderr,"temp5=%s\n",temp5);	*/
 
   mysql_init(&my_connection);
 
   if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", databas, 0, NULL, 0)){
-  // fprintf(stdout,"SLPADD:Connection success\n");
+/* fprintf(stdout,"SLPADD:Connection success\n");	*/
 
     res = mysql_query(&my_connection,temp5);
 
@@ -177,7 +182,6 @@ int which_database(char *envp[])
 {
 	FILE *fil_pek;
 
-//	char home[]="$HOME";
 	char home[50];
 	char *home_pek;
 	char resource[]="/.olfixrc";
@@ -190,47 +194,48 @@ int which_database(char *envp[])
 	for (i = 0;envp[i]!=NULL;i++){
 		if(strstr(envp[i],"HOME=") != NULL){
 			strncpy(temp,envp[i],4);
-//			fprintf(stderr,"temp=%s\n",temp);
+/*			fprintf(stderr,"temp=%s\n",temp); */
 			status=strcmp(temp,"HOME");
-//			fprintf(stderr,"status=%d\n",status);
+/*			fprintf(stderr,"status=%d\n",status); */
 			if (status == 0){
 				home_pek=(strstr(envp[i],"HOME="));
 				home_pek=home_pek+5;
 				strcpy(home,home_pek);
 			}
-//			fprintf(stderr,"home_pek=%d %s\n",home_pek,home_pek);
-//			fprintf(stderr,"home_pek=%d %s\n",home_pek,home_pek);
+/*			fprintf(stderr,"home_pek=%d %s\n",home_pek,home_pek);	*/
 		}
 	}
-//	fprintf(stderr,"home=%s\n",home);
-	strcpy(filename,home);
-	strcat(filename,resource);
+/*	fprintf(stderr,"home=%s\n",home);	*/
+	strncpy(filename,home,strlen(home));
+	strncat(filename,resource,strlen(resource));
 
-//	fprintf(stderr,"filename=%s\n",filename);
+/*	fprintf(stderr,"filename=%s\n",filename);	*/
 	status=-1;
 
 	if ((fil_pek = fopen(filename,"r")) != NULL){
 		while (fgets(tmp,150,fil_pek) != NULL){
-//			fprintf(stderr,"tmp=%s\n",tmp);
+/*			fprintf(stderr,"tmp=%s\n",tmp); */
 			if(strstr(tmp,"DATABASE=")){
 				tmp_pek=(strstr(tmp,"DATABASE="))+9;
 				strncpy(database,tmp_pek,strlen(tmp_pek));
 				status=0;
 			}
 		}
-//		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database));
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
 		fclose(fil_pek);
 	}
 	else{
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
 	 	fprintf(stderr,"Error: Filen .olfixrc kan inte öppnas\n");
 	}
 	for (i=0;i < strlen(database);i++){
 		tmp[i]=database[i];
 	}
 	tmp[i-1]=0;
-//	fprintf(stderr,"tmp=%s, i=%d len tmp=%d\n",tmp,i,strlen(tmp));
+/*	fprintf(stderr,"tmp=%s, i=%d len(tmp)=%d\n",tmp,i,strlen(tmp));	*/
 	strncpy(database,tmp,strlen(tmp));
 	database[strlen(tmp)]=0;
+/*	fprintf(stderr,"databas=%s\n",database);	*/
 
 	return status;
 }
