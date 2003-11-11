572d2d440b08add123ab2922277e183a5aa2fc2d
janpihlgren <janpihlgren>
2003-11-11 05:39:43 +0000

    Lagt in val av företag samt kodstädning

--- prototype/src/VALADD.c
+++ prototype/src/VALADD.c
@@ -1,7 +1,9 @@
 /***************************************************************************
                           VALADD.c  -  description
                              -------------------
-    begin                : Fre 21 febr
+    Version		 : 0.2
+    begin                : Fre 21 febr	2003
+    modified		 : Tis 11 nov 2003
     copyright            : (C) 2003 by Jan Pihlgren
     email                : jan@pihlgren.se
  ***************************************************************************/
@@ -26,7 +28,7 @@
 
 */
  /*@unused@*/ static char RCS_id[] =
-    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/VALADD.c,v 1.1 2003/05/08 08:54:12 frazze Exp $ " ;
+    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/VALADD.c,v 1.2 2003/11/11 05:39:43 janpihlgren Exp $ " ;
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -35,68 +37,113 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "mysql.h"
+#define ANTARG 6
 
-
-int main(int argc, char *argv[])
-{
   MYSQL my_connection;
+  MYSQL_RES *res_ptr;
+  MYSQL_ROW sqlrow;
+
+  int which_database(char *envp[]);
+  char database[15]="";
+
+int main(int argc, char *argv[], char *envp[])
+{
+/* int i;	*/
   int res;
+  int status;
+  const char *userp = getenv("USER");	/* vem är inloggad?	*/
+  char databas[25]="olfix";
+  char usr[15];				/* userid		*/
 
   char temp1[]="INSERT INTO VALUTA(VALUTAID,LAND,SALJ,KOP,BETECKNING) VALUES (\"";
   char temp2[]="\"";
   char temp3[]=",";
   char temp4[]=")";
   char temp5[200]="";
-  char valuta[4];
-  char land[16];
-  char salj[15];
-  char kop[15];
-  char beteckning[16];
+  char valuta[4]="";
+  char land[16]="";
+  char salj[15]="";
+  char kop[15]="";
+  char beteckning[16]="";
 
-  strcpy(valuta,argv[1]);
-  strcpy(land,argv[2]);
-  strcpy(salj,argv[3]);
-  strcpy(kop,argv[4]);
-  strcpy(beteckning,argv[5]);
+/* ================================================================================ */
+/* 		Val av databas, START						    */
+/* ================================================================================ */
 
+  status = which_database(envp);
 
-  strcat(temp5,temp1);
+  if (status != 0)
+	exit(status);
+
+  strncpy(usr,userp,15);			/* Den inloggades userid	*/
+/*  fprintf(stderr,"status=%d ANTARG=%d len(database)=%d\n",status,ANTARG,strlen(database));	*/
+  if (argc < ANTARG+1){
+    	if (strlen(database)!= 0){
+		strncpy(databas,database,15);
+	}else{
+  		strncpy(databas,"olfixtst",15);	/* olfixtst = testföretag	*/
+	}
+  }else{
+	if (strlen(argv[ANTARG]) != 0){
+  		if (strncmp(argv[ANTARG],"99",2)==0){
+			strncpy(databas,"olfixtst",15);
+		}else{
+  			strncpy(databas,argv[ANTARG],15);
+  		}
+  	}
+  }
+/*  fprintf(stderr,"ANTARG=%d,argv[ANTARG]=%s\n",ANTARG,argv[ANTARG]);	*/
+/* Om usr (userid) börjar på 'test' eller 'prov' använd databas 'olfixtst' */
+  if (strncmp(usr,"test",4)==0 || strncmp(usr,"prov",4)==0 ) {
+  	strncpy(databas,"olfixtst",15);
+  }
+/* fprintf(stderr,"Databas=%s\n",databas);	*/
+/* ================================================================================ */
+/* 		Val av databas, END!						    */
+/* ================================================================================ */
+
+  strncpy(valuta,argv[1],strlen(argv[1]));
+  strncpy(land,argv[2],strlen(argv[2]));
+  strncpy(salj,argv[3],strlen(argv[3]));
+  strncpy(kop,argv[4],strlen(argv[4]));
+  strncpy(beteckning,argv[5],strlen(argv[5]));
+
+  strncpy(temp5,temp1,strlen(temp1));
 /* INSERT INTO VALUTA(VALUTAID,LAND,SALJ,KOP,BETECKNING) VALUES ("  */
-  strcat(temp5,valuta);/* SEK */
+  strncat(temp5,valuta,strlen(valuta));/* SEK */
 /* INSERT INTO VALUTA(VALUTAID,LAND,SALJ,KOP,BETECKNING) VALUES ("SEK  */
-  strcat(temp5,temp2); /*  "     */
-  strcat(temp5,temp3); /*  ,     */
-  strcat(temp5,temp2); /*  "     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
+  strncat(temp5,temp3,strlen(temp3)); /*  ,     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
 /* INSERT INTO VALUTA(VALUTAID,LAND,SALJ,KOP,BETECKNING) VALUES ("SEK","  */
-  strcat(temp5,land);/* Sverige  */
+  strncat(temp5,land,strlen(land));/* Sverige  */
 /* INSERT INTO VALUTA(VALUTAID,LAND,SALJ,KOP,BETECKNING) VALUES ("SEK","Sverige  */
-  strcat(temp5,temp2); /*  "     */
-  strcat(temp5,temp3); /*  ,     */
-  strcat(temp5,temp2); /*  "     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
+  strncat(temp5,temp3,strlen(temp3)); /*  ,     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
 /* INSERT INTO VALUTA(VALUTAID,LAND,SALJ,KOP,BETECKNING) VALUES ("SEK","Sverige","  */
-  strcat(temp5,salj);
+  strncat(temp5,salj,strlen(salj));
 /* INSERT INTO VALUTA(VALUTAID,LAND,SALJ,KOP,BETECKNING) VALUES ("SEK","Sverige","1.22   */
-  strcat(temp5,temp2); /*  "     */
-  strcat(temp5,temp3); /*  ,     */
-  strcat(temp5,temp2); /*  "     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
+  strncat(temp5,temp3,strlen(temp3)); /*  ,     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
 /* INSERT INTO VALUTA(VALUTAID,LAND,SALJ,KOP,BETECKNING) VALUES ("SEK","Sverige","1.22","  */
-  strcat(temp5,kop);
+  strncat(temp5,kop,strlen(kop));
 /* INSERT INTO VALUTA(VALUTAID,LAND,SALJ,KOP,BETECKNING) VALUES ("SEK","Sverige","1.22","1.23  */
-  strcat(temp5,temp2); /*  "     */
-  strcat(temp5,temp3); /*  ,     */
-  strcat(temp5,temp2); /*  "     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
+  strncat(temp5,temp3,strlen(temp3)); /*  ,     */
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
 /* INSERT INTO VALUTA(VALUTAID,LAND,SALJ,KOP,BETECKNING) VALUES ("SEK","Sverige","1.22","1.23"," */
-  strcat(temp5,beteckning);
-  strcat(temp5,temp2); /*  "     */
-  strcat(temp5,temp4); /*  )     */
+  strncat(temp5,beteckning,strlen(beteckning));
+  strncat(temp5,temp2,strlen(temp2)); /*  "     */
+  strncat(temp5,temp4,strlen(temp4)); /*  )     */
 /* INSERT INTO VALUTA(VALUTAID,LAND,SALJ,KOP,BETECKNING) VALUES ("SEK","Sverige","1.22","1.23","Kronor")  */
 
   mysql_init(&my_connection);
 
-  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", "olfix", 0, NULL, 0)){
-  // fprintf(stdout,"USERADD:Connection success\n");
+  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", databas, 0, NULL, 0)){
+/* fprintf(stdout,"USERADD:Connection success\n");		*/
 
     res = mysql_query(&my_connection,temp5);
 
@@ -120,4 +167,64 @@ int main(int argc, char *argv[])
   return EXIT_SUCCESS;
 }
 
+int which_database(char *envp[])
+{
+	FILE *fil_pek;
 
+	char home[50];
+	char *home_pek;
+	char resource[]="/.olfixrc";
+	char filename[50]="";
+	char tmp[20]="";
+	char temp[10]="";
+	char *tmp_pek;
+	int i,status;
+
+	for (i = 0;envp[i]!=NULL;i++){
+		if(strstr(envp[i],"HOME=") != NULL){
+			strncpy(temp,envp[i],4);
+/*			fprintf(stderr,"temp=%s\n",temp); */
+			status=strcmp(temp,"HOME");
+/*			fprintf(stderr,"status=%d\n",status); */
+			if (status == 0){
+				home_pek=(strstr(envp[i],"HOME="));
+				home_pek=home_pek+5;
+				strcpy(home,home_pek);
+			}
+/*			fprintf(stderr,"home_pek=%d %s\n",home_pek,home_pek);	*/
+		}
+	}
+/*	fprintf(stderr,"home=%s\n",home);	*/
+	strncpy(filename,home,strlen(home));
+	strncat(filename,resource,strlen(resource));
+
+/*	fprintf(stderr,"filename=%s\n",filename);	*/
+	status=-1;
+
+	if ((fil_pek = fopen(filename,"r")) != NULL){
+		while (fgets(tmp,150,fil_pek) != NULL){
+/*			fprintf(stderr,"tmp=%s\n",tmp); */
+			if(strstr(tmp,"DATABASE=")){
+				tmp_pek=(strstr(tmp,"DATABASE="))+9;
+				strncpy(database,tmp_pek,strlen(tmp_pek));
+				status=0;
+			}
+		}
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
+		fclose(fil_pek);
+	}
+	else{
+/*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
+	 	fprintf(stderr,"Error: Filen .olfixrc kan inte öppnas\n");
+	}
+	for (i=0;i < strlen(database);i++){
+		tmp[i]=database[i];
+	}
+	tmp[i-1]=0;
+/*	fprintf(stderr,"tmp=%s, i=%d len(tmp)=%d\n",tmp,i,strlen(tmp));	*/
+	strncpy(database,tmp,strlen(tmp));
+	database[strlen(tmp)]=0;
+/*	fprintf(stderr,"databas=%s\n",database);	*/
+
+	return status;
+}
