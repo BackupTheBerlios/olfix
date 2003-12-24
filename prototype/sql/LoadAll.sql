dfd9a9481cf9c8c1b70cd92ed1246faa446972d2
janpihlgren <janpihlgren>
2003-12-24 03:38:10 +0000

    Kompleterat med olfixtst

--- prototype/sql/LoadAll.sql
+++ prototype/sql/LoadAll.sql
@@ -5,8 +5,13 @@ load data infile "/usr/local/olfix/data/TRANSIDdata.txt" replace into table TRAN
 load data infile "/usr/local/olfix/data/VALUTAdata.txt" replace into table VALUTA Fields terminated by ',' enclosed by '"';
 load data infile "/usr/local/olfix/data/PROGRAMdata.txt" replace into table PROGRAM Fields terminated by ',' enclosed by '"';
 use olfixtst;
-load data infile "/usr/local/olfix/data/FTGDATAdata.txt" replace into table FTGDATA Fields terminated by ',' enclosed by '"';
+load data infile "/usr/local/olfix/data/FTGDATAdata_olfixtst.txt" replace into table FTGDATA Fields terminated by ',' enclosed by '"';
 load data infile "/usr/local/olfix/data/KTOPLANdata.txt" replace into table KTOPLAN Fields terminated by ',' enclosed by '"';
 load data infile "/usr/local/olfix/data/TRANSIDdata.txt" replace into table TRANSID Fields terminated by ',' enclosed by '"';
 load data infile "/usr/local/olfix/data/VALUTAdata.txt" replace into table VALUTA Fields terminated by ',' enclosed by '"';
 load data infile "/usr/local/olfix/data/PROGRAMdata.txt" replace into table PROGRAM Fields terminated by ',' enclosed by '"';
+load data infile "/usr/local/olfix/data/KUNDREGdata_olfixtst.txt" replace into table KUNDREG Fields terminated by ',' enclosed by '"';
+load data infile "/usr/local/olfix/data/LEVREGdata_olfixtst.txt" replace into table LEVREG Fields terminated by ',' enclosed by '"';
+load data infile "/usr/local/olfix/data/BETVILKORdata_olfixtst.txt" replace into table BETVILKOR Fields terminated by ',' enclosed by '"';
+load data infile "/usr/local/olfix/data/ARTIKELREGdata_olfixtst.txt" replace into table ARTIKELREG Fields terminated by ',' enclosed by '"';
+load data infile "/usr/local/olfix/data/LAGERSTELLEREGdata_olfixtst.txt" replace into table LAGERSTELLEREG Fields terminated by ',' enclosed by '"';
