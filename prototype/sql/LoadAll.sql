136a8b8fa95338de280ffbe119eacaf4de56ffe2
janpihlgren <janpihlgren>
2003-12-24 03:32:16 +0000

    Kompleterat med olfixtst

--- prototype/sql/LoadAll.sql
+++ prototype/sql/LoadAll.sql
@@ -4,4 +4,9 @@ load data infile "/usr/local/olfix/data/KTOPLANdata.txt" replace into table KTOP
 load data infile "/usr/local/olfix/data/TRANSIDdata.txt" replace into table TRANSID Fields terminated by ',' enclosed by '"';
 load data infile "/usr/local/olfix/data/VALUTAdata.txt" replace into table VALUTA Fields terminated by ',' enclosed by '"';
 load data infile "/usr/local/olfix/data/PROGRAMdata.txt" replace into table PROGRAM Fields terminated by ',' enclosed by '"';
-
+use olfixtst;
+load data infile "/usr/local/olfix/data/FTGDATAdata.txt" replace into table FTGDATA Fields terminated by ',' enclosed by '"';
+load data infile "/usr/local/olfix/data/KTOPLANdata.txt" replace into table KTOPLAN Fields terminated by ',' enclosed by '"';
+load data infile "/usr/local/olfix/data/TRANSIDdata.txt" replace into table TRANSID Fields terminated by ',' enclosed by '"';
+load data infile "/usr/local/olfix/data/VALUTAdata.txt" replace into table VALUTA Fields terminated by ',' enclosed by '"';
+load data infile "/usr/local/olfix/data/PROGRAMdata.txt" replace into table PROGRAM Fields terminated by ',' enclosed by '"';
