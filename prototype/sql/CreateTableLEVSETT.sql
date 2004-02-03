0fec8994497014dfa0479984452b5efcc819afec
janpihlgren <janpihlgren>
2004-02-03 04:36:25 +0000

    Nytabell LEVSETT

---
+++ prototype/sql/CreateTableLEVSETT.sql
@@ -0,0 +1,7 @@
+use olfix;
+create table LEVSETT(LEVSETTNR VARCHAR(3) not null PRIMARY KEY,LEVSETTTEXT VARCHAR(150)not null );
+use olfixtst;
+create table LEVSETT(LEVSETTNR VARCHAR(3) not null PRIMARY KEY,LEVSETTTEXT VARCHAR(150)not null );
+
+
+
