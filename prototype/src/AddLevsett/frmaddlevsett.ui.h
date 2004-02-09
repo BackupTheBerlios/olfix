48dd941d70dd6048cd2aa2d49094591265419071
janpihlgren <janpihlgren>
2004-02-09 06:04:03 +0000

    Nytt program ADDLEVSW

---
+++ prototype/src/AddLevsett/frmaddlevsett.ui.h
@@ -0,0 +1,140 @@
+/****************************************************************************
+** ui.h extension file, included from the uic-generated form implementation.
+**
+** If you wish to add, delete or rename slots use Qt Designer which will
+** update this file, preserving your code. Create an init() slot in place of
+** a constructor, and a destroy() slot in place of a destructor.
+*****************************************************************************/
+/***************************************************************************
+                          ADDLEVSW  -  description
+                             ---------------
+    version	: 0.1
+    begin                   : Mån 9 febr 2004
+    copyright            : (C) 2003 by Jan Pihlgren
+    email                : jan@pihlgren.se
+ ***************************************************************************/
+/*****************************************************************
+ *					                                                 *
+ *   This program is free software; you can redistribute it and/or modify 	 *
+ *   it under the terms of the GNU General Public License as published by       *
+ *   the Free Software Foundation; either version 2 of the License, or     	 *
+ *   (at your option) any later version.                                   		 *
+ *                                                                         				 *
+ *********************************************** *****************/
+#include <qmessagebox.h>
+#include <qprocess.h>
+#include <qdir.h>
+#include <stdlib.h>
+#include <qstring.h>		
+#include <qfile.h>
+#include <qregexp.h>
+#define MAXSTRING 5000
+
+    QProcess* process;
+    QString inrad;
+    QString* rad;
+    QString errorrad;
+    QString levsett;
+    QString dagar;
+    QString beskrivning;
+    
+    
+void frmAddLevsett::init()
+{
+    LineEditLevsett->setFocus();
+}
+
+void frmAddLevsett::LineEditLevsett_returnPressed()
+{
+    levsett=LineEditLevsett->text();
+    LineEditTextLevsett->setFocus();
+}
+
+void frmAddLevsett::LineEditTextLevsett_returnPressed()
+{
+    beskrivning=LineEditTextLevsett->text();
+    PushButtonOK->setFocus();
+}
+
+void frmAddLevsett::PushButtonOK_clicked()
+{
+    frmAddLevsett::slotAddLevsett();
+}
+
+void frmAddLevsett::slotAddLevsett()
+{
+/************************************************************************/
+/*	Uppdatera databasen						*/
+/************************************************************************/
+	const char *userp = getenv("USER");
+            QString usr(userp);
+	   
+	process = new QProcess();
+	process->addArgument("./STYRMAN");	// OLFIX styrprogram
+	process->addArgument(usr);		// userid
+	process->addArgument( "LEVSADD");	// OLFIX funktion
+	process->addArgument(levsett);
+	process->addArgument(beskrivning);
+	
+	frmAddLevsett::connect( process, SIGNAL(readyReadStdout() ),this, SLOT(slotDataOnStdout() ) );
+	frmAddLevsett::connect( process, SIGNAL(readyReadStderr() ),this, SLOT(slotDataOnStderr() ) );
+            frmAddLevsett::connect( process, SIGNAL(processExited() ),this, SLOT(slotEndOfProcess() ) );
+ 
+	if (levsett == "" ){
+    	    QMessageBox::warning( this, "ADDLEVSW",
+                      "Leveransvillkor saknas \n" );
+	    LineEditLevsett->setFocus();
+	}
+	else {
+	    if ( !process->start() ) {
+		// error handling
+		QMessageBox::warning( this, "ADDLEVSW",
+                            "Kan inte starta STYRMAN/LEVVADD! \n" );
+	    }   
+	}
+}
+
+void frmAddLevsett::slotDataOnStdout()
+{
+    while (process->canReadLineStdout() ) {
+	QString line = process->readStdout();
+	inrad.append(line);
+	inrad.append("\n");
+    }
+}
+
+void frmAddLevsett::slotDataOnStderr()
+{
+    while (process->canReadLineStderr() ) {
+	QString line = process->readStderr();
+	errorrad.append(line);
+	errorrad.append("\n");
+    }
+}
+
+void frmAddLevsett::slotEndOfProcess()
+{
+    int i;
+    i = -1;
+    i = errorrad.find( QRegExp("Error:"), 0 );
+ //   qDebug("Error:",errorrad);
+         if (i != -1) {
+	QMessageBox::critical( this, "ADDLEVSW",
+		"ERROR!\n"+errorrad
+	);
+	errorrad="";
+	i = -1;
+     }
+     i = -1;
+     i = inrad.find( QRegExp("OK:"), 0 );
+     if (i != -1) {
+	QMessageBox::information( this, "ADDLEVSW",
+		"Uppdatering OK!\n"+errorrad
+	);
+	LineEditLevsett->clear();
+	LineEditTextLevsett->clear();
+	LineEditLevsett->setFocus();
+	inrad="";
+	i = -1;
+     }
+}
