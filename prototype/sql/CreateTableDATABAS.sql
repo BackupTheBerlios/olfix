873a6fb12acc0b3440543c5200ab1f4565c12487
janpihlgren <janpihlgren>
2004-11-09 19:06:16 +0000

    Skapa tabellen DATABAS

---
+++ prototype/sql/CreateTableDATABAS.sql
@@ -0,0 +1,7 @@
+use olfix;
+create table DATABAS(DATABASNR VARCHAR(3) not null PRIMARY KEY,DATABASTEXT VARCHAR(15)not null );
+use olfixtst;
+create table DATABAS(DATABASNR VARCHAR(3) not null PRIMARY KEY,DATABASTEXT VARCHAR(15)not null );
+
+
+
