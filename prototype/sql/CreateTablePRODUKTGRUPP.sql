8ba62bf3ba8d570fc854836d79aa55efea4fe452
janpihlgren <janpihlgren>
2004-11-28 08:43:22 +0000

    Ny tabell,PRODUKTGRUPP

---
+++ prototype/sql/CreateTablePRODUKTGRUPP.sql
@@ -0,0 +1,11 @@
+# Connection: olfix
+# Host: localhost
+# Saved: 2004-11-28 09:33:53
+# 
+use olfix;
+create table PRODUKTGRUPP(PRODKLASS VARCHAR(5) not null PRIMARY KEY,BESKRIVNING VARCHAR(30) );
+use olfixtst;
+create table PRODUKTGRUPP(PRODKLASS VARCHAR(5) not null PRIMARY KEY,BESKRIVNING VARCHAR(30));
+
+
+
