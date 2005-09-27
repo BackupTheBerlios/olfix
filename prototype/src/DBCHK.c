d21e951752e1e1ccf3e2340949ba6588872d1b4c
janpihlgren <janpihlgren>
2005-09-27 08:09:46 +0000

    Visa vilka databaser som finns i systemet

---
+++ prototype/src/DBCHK.c
@@ -0,0 +1,191 @@
+/***************************************************************************
+                          DBCHK.c  -  description
+                             -------------------
+			     Version: 0.1
+    begin                : Tis 27 sept	2005
+    Modified		 :
+    copyright            : (C) 2005 by Jan Pihlgren
+    email                : jan@pihlgren.se
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+/*
+	INPUT:
+
+	Kommando: ./DBCHK
+
+	Function: Lista vilka databaser som finns i systemet
+
+	OUTPUT: data, errornb och error (text)
+
+*/
+ /*@unused@*/ static char RCS_id[] =
+    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/DBCHK.c,v 1.1 2005/09/27 08:09:46 janpihlgren Exp $ " ;
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "mysql.h"
+  MYSQL my_connection;
+  MYSQL_RES *res_ptr;
+  MYSQL_ROW sqlrow;
+  void display_row();
+  int which_database(char *envp[]);
+
+  char database[15]="";
+
+int main(int argc, char *argv[], char *envp[])
+{
+  int res,status;
+  int i;
+
+  char temp1[]="SELECT db FROM db";
+  char temp5[200]="";
+  const char *userp = getenv("USER");	/* vem är inloggad?	*/
+  char databas[25]="olfix";
+  char usr[15];				/* userid		*/
+
+  strncpy(usr,userp,15);			/* Den inloggades userid	*/
+
+  if (argc<3){
+    	if (strlen(database)!= 0){
+		strncpy(databas,database,15);
+	}else{
+  		strncpy(databas,"olfixtst",15);	/* olfixtst = testföretag, används som grundvärde	*/
+	}
+  }else{
+	if (strlen(argv[2]) != 0){
+  		if (strncmp(argv[2],"99",2)==0){
+			strncpy(databas,"olfixtst",15);
+		}else{
+  			strncpy(databas,argv[2],15);
+  		}
+  	}
+  }
+  /* Om usr (userid) börjar på 'test' eller 'prov' använd databas 'olfixtst' */
+  if (strncmp(usr,"test",4)==0 || strncmp(usr,"prov",4)==0 ) {
+  	strncpy(databas,"olfixtst",15);
+  }
+//  fprintf(stderr,"ARCHK database = %s databas=%s\n",database,databas);
+
+// ================================================================================
+// 		Val av databas, END!
+// ================================================================================
+
+  strcpy(temp5,temp1);
+/* SELECT db FROM db  */
+
+  mysql_init(&my_connection);
+/* anropa databasen mysql som innehåller en förteckning av befintliga databaset i systemet.	*/
+  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", "mysql", 0, NULL, 0)){
+/*  fprintf(stdout,"DBCHKmain:Connection success\n");		*/
+  	res = mysql_query(&my_connection,temp5);
+  	if (res){
+		fprintf(stderr,"Error: DBCHK SELECT error: %s\n",mysql_error(&my_connection));
+  	}else{
+		res_ptr=mysql_store_result(&my_connection);
+		if (res_ptr){
+//			fprintf(stderr,"DBCHK_Retrieved %lu rows\n",(unsigned long)mysql_num_rows(res_ptr));
+			if((unsigned long)mysql_num_rows(res_ptr) > 0){ /* En rad har hittats */
+				status=0;
+			}else{						/* Ingen rad hittades */
+				status=-1;
+			}
+			i=1;
+			fprintf(stdout,"NR_%lu_:",(unsigned long)mysql_num_rows(res_ptr));
+			while ((sqlrow=mysql_fetch_row(res_ptr)))  {
+				display_row();
+				i++;
+			}
+			fprintf(stdout,"\n");
+			if (mysql_errno(&my_connection)){
+				fprintf(stderr,"Error: DBCHK Retriev error:  %s\n", mysql_error(&my_connection));
+			}
+		}
+		mysql_free_result(res_ptr);
+	}
+  	mysql_close(&my_connection);
+ }else{
+  	fprintf(stderr,"Error: DBCHK Connection failed\n");
+	if (mysql_errno(&my_connection)){
+		fprintf(stderr,"Error: DBCHK Connection error %d:  %s\n",
+		mysql_errno(&my_connection), mysql_error(&my_connection));
+	}
+  }
+  return EXIT_SUCCESS;
+}
+
+void display_row()
+{
+   unsigned int field_count;
+   field_count=0;
+	while (field_count < mysql_field_count(&my_connection))
+	{
+		fprintf(stdout,"%s_:",sqlrow[field_count]);
+		field_count++;
+	}
+}
+
+int which_database(char *envp[])
+{
+	FILE *fil_pek;
+
+	char home[50];
+	char *home_pek;
+	char resource[]="/.olfixrc";
+	char filename[50]="";
+	char tmp[20]="";
+	char temp[10]="";
+	char *tmp_pek;
+	int i,status;
+
+	for (i = 0;envp[i]!=NULL;i++){
+		if(strstr(envp[i],"HOME=") != NULL){
+			strncpy(temp,envp[i],4);
+			status=strcmp(temp,"HOME");
+			if (status == 0){
+				home_pek=(strstr(envp[i],"HOME="));
+				home_pek=home_pek+5;
+				strcpy(home,home_pek);
+			}
+		}
+	}
+	strncpy(filename,home,strlen(home));
+	strncat(filename,resource,strlen(resource));
+
+	status=-1;
+
+	if ((fil_pek = fopen(filename,"r")) != NULL){
+		while (fgets(tmp,150,fil_pek) != NULL){
+			if(strstr(tmp,"DATABASE=")){
+				tmp_pek=(strstr(tmp,"DATABASE="))+9;
+				strncpy(database,tmp_pek,strlen(tmp_pek));
+				status=0;
+			}
+		}
+		fclose(fil_pek);
+	}
+	else{
+	 	fprintf(stderr,"Error: Filen .olfixrc kan inte öppnas\n");
+	}
+	for (i=0;i < strlen(database);i++){
+		tmp[i]=database[i];
+	}
+	tmp[i-1]=0;
+	strncpy(database,tmp,strlen(tmp));
+	database[strlen(tmp)]=0;
+	return status;
+}
