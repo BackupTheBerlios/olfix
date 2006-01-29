ad88b4310ad9654ee839286c02e5cf4a6abb1efa
janpihlgren <janpihlgren>
2006-01-29 08:12:18 +0000

    Uppdatera priser, CHGPRISW

--- prototype/src/ChangePrislista/frmloadprices.ui.h
+++ prototype/src/ChangePrislista/frmloadprices.ui.h
@@ -35,7 +35,7 @@
 #include <qstring.h>
 #include <qfile.h>
 #include <qregexp.h>
-#define MAXSTRING 5000
+#define MAXSTRING 25000
 
 QProcess* process;
 QString inrad;
@@ -43,28 +43,38 @@ QString errorrad;
 QString hjelpfil;
 QString database;
 QString pricefilename;
-QString prislista="A";
+QString tmpfilename;
+QString prislista="1";
 
-bool testflag=FALSE;
+bool testflag=TRUE;
+bool running=FALSE;
 
 void frmLoadPrices::init()
 {
+    prislista="1";
+    pushButtonCancel->setText("Avbryt");
     frmLoadPrices::readResursFil();
     lineEditFilname->setFocus();
 }
 
 void frmLoadPrices::lineEditFilname_returnPressed()
 {
+    pricefilename="";
     pricefilename=lineEditFilname->text();
-    pushButtonTest->setFocus();
+    running=FALSE;
+    return;
+//    pushButtonTest->setFocus();
 }
 
 void frmLoadPrices::pushButtonOK_clicked()
 {
     testflag=FALSE;
+    running=TRUE;
     listViewResultat->clear();
-//    getPricefile();
-    updatePrices();
+    getPricefile();
+    qDebug("pushButtonOK: tmpfil=%s prislista=%s",tmpfilename.latin1(),prislista.latin1());
+    updatePrices(tmpfilename,prislista);
+    pushButtonCancel->setFocus();
 }
 
 void frmLoadPrices::pushButtonCancel_clicked()
@@ -121,23 +131,32 @@ void frmLoadPrices::radioButtonF_clicked()
     prislista="F";
 }
 
-void frmLoadPrices::updatePrices()
+void frmLoadPrices::getPricefile()
 {
-//    bool load=FALSE;
     QListViewItem * item;
     QStringList lines;
     QString line;
     QString record;
     int recordsize;
     QString delemit=";";
-    QString tmpfilename="/home/jan/tmp/pricefile.tmp";
-//    QString loadfile="/home/jan/tmp/loadfile.sql";
-    QFile pricefile( pricefilename );
-    QFile tempfile(tmpfilename);
+    QString homepath;
+    homepath=QDir::homeDirPath();
+
+    tmpfilename=homepath;				/* $HOME */
+    QString infile=homepath;
+    infile=infile.append("/tmp/");			/* $HOME/tmp/ */
+    infile=infile.append(pricefilename);			/* Infil, $HOME/tmp/nyaprisfilen*/
+    
+    tmpfilename=tmpfilename.append("/tmp/pricefile.tmp");	/* Utfil, $HOME/tmp/pricefile.tmp */
+    
+    QFile pricefile(infile );				/* Infil, den nya prisfilen */
+    QFile tempfile(tmpfilename);			/* Utfil */
     ulong offset=0;
     
-//    qDebug("prislista=%s",prislista.latin1());
-//    qDebug("tmpfilename=%s",tmpfilename.latin1());
+//   qDebug("prislista=%s",prislista.latin1());
+//   qDebug("tmpfilename=%s",tmpfilename.latin1());
+//   qDebug("pricefilename=%s",infile.latin1());
+    
     tempfile.open( IO_WriteOnly );
     if (pricefile.open( IO_ReadOnly ) ) {
        QTextStream stream( &pricefile );
@@ -187,27 +206,27 @@ void frmLoadPrices::updatePrices()
 	    if (testflag == TRUE){
 		item = new QListViewItem(listViewResultat,artnr,fpris,pris1,pris2,pris3,pris4,pris5);
 	    }else{
-		qDebug("pris1=%s pris2=%s pris3=%s pris4=%s pris5=%s",pris1.latin1(),pris2.latin1(),pris3.latin1(),pris4.latin1(),pris5.latin1());
+//		qDebug("pris1=%s pris2=%s pris3=%s pris4=%s pris5=%s",pris1.latin1(),pris2.latin1(),pris3.latin1(),pris4.latin1(),pris5.latin1());
 		if (prislista=="A"){
-		    record=delemit+artnr+delemit+pris1+delemit+pris2+delemit+pris3+delemit+pris4+delemit+pris5+delemit+"\n";
+		    record=artnr+delemit+pris1+delemit+pris2+delemit+pris3+delemit+pris4+delemit+pris5+"\n";
 		}
 		if (prislista=="1"){
-		    record=delemit+artnr+delemit+pris1+delemit+"\n";
+		    record=artnr+delemit+pris1+"\n";
 		}
 		if (prislista=="2"){
-		    record=delemit+artnr+delemit+pris2+delemit+"\n";
+		    record=artnr+delemit+pris2+"\n";
 		}
 		if (prislista=="3"){
-		    record=delemit+artnr+delemit+pris3+delemit+"\n";
+		    record=artnr+delemit+pris3+"\n";
 		}
 		if (prislista=="4"){
-		    record=delemit+artnr+delemit+pris4+delemit+"\n";
+		    record=artnr+delemit+pris4+"\n";
 		}
 		if (prislista=="5"){
-		    record=delemit+artnr+delemit+pris5+delemit+"\n";
+		    record=artnr+delemit+pris5+"\n";
 		}
 		if (prislista=="F"){
-		    record=delemit+artnr+delemit+fpris+delemit+"\n";
+		    record=artnr+delemit+fpris+"\n";
 		}				
 		recordsize=record.length();
 		tempfile.at( offset );
@@ -220,60 +239,90 @@ void frmLoadPrices::updatePrices()
         }
     }
    pricefile.close();
-   tempfile.close();
+   tempfile.close();  
+   return;
 }
 
-void frmLoadPrices::getPricefile()
+void frmLoadPrices::updatePrices(QString file, QString price)
 {
-    QStringList lines;
-    QString line;    
-    bool load=FALSE;
-    QListViewItem * item;
-    
-    QFile pricefile( pricefilename );
-     if (pricefile.open( IO_ReadOnly ) ) {
-        QTextStream stream( &pricefile );
-//	qDebug("load=%d",load);
-        while ( !stream.eof() && load == FALSE) {
-                line = stream.readLine(); 			/* line of text excluding '\n'	*/
-//	qDebug("line=%s",line.latin1());
-	int i1a=line.find( QRegExp("\""), 0 );		/* Artnr */
-	int i1b=line.find( QRegExp("\""), i1a+1 );
-	int i2a=line.find( QRegExp("\""), i1b+1 );		/* Fpris */
-	int i2b=line.find( QRegExp("\""), i2a+1 );
-	int i3a=line.find( QRegExp("\""), i2b+1 );		/* Pris1 */
-	int i3b=line.find( QRegExp("\""), i3a+1 );
-	int i4a=line.find( QRegExp("\""), i3b+1 );		/* Pris2 */
-	int i4b=line.find( QRegExp("\""), i4a+1 );
-	int i5a=line.find( QRegExp("\""), i4b+1 );		/* Pris3 */
-	int i5b=line.find( QRegExp("\""), i5a+1 );
-	int i6a=line.find( QRegExp("\""), i5b+1 );		/* Pris4 */
-	int i6b=line.find( QRegExp("\""), i6a+1 );
-	int i7a=line.find( QRegExp("\""), i6b+1 );		/* Pris5 */
-	int i7b=line.find( QRegExp("\""), i7a+1 );
+/************************************************************************/
+/*	Uppdatera databasen					                  */
+/************************************************************************/
+    inrad="";
+    errorrad="";
+    const char *userp = getenv("USER");
+    QString usr(userp);
+    if (running != FALSE){
+ //   QMessageBox::information(this,"CHGPRISW","Starta PRISUPD!");
+    qDebug("updatePrices: tmpfil=%s prislista=%s",tmpfilename.latin1(),price.latin1());	   	    
+    process = new QProcess();
+    process->addArgument("./STYRMAN");	// OLFIX styrprogram
+    process->addArgument(usr);		// Userid
+    process->addArgument( "PRISUPD");		// OLFIX funktion
+    process->addArgument(file);
+    process->addArgument(price);
+	
+    frmLoadPrices::connect( process, SIGNAL(readyReadStdout() ),this, SLOT(slotDataOnStdout() ) );
+    frmLoadPrices::connect( process, SIGNAL(readyReadStderr() ),this, SLOT(slotDataOnStderr() ) );
+    frmLoadPrices::connect( process, SIGNAL(processExited() ),this, SLOT(slotEndOfProcess() ) );
+ 
+	
+    if (pricefilename== "") {
+	QMessageBox::warning( this, "CHGPRISW","Filnamn saknas! \n" );
+    }else {
+	if ( !process->start() ) {
+	// error handling
+//	fprintf(stderr,"Problem starta STYRMAN/PRISUPD!\n");
+	QMessageBox::warning( this, "CHGPRISW",
+                           "Kan inte starta STYRMAN/PRISUPD! \n" );
+                }   
+  }
+    qDebug("Startat PRISUPD!");
+    running=FALSE;
+    pushButtonCancel->setFocus();
+ }
+}
 
-	if(i1a == 0){
-	    QString artnr=line.mid(i1a+1,(i1b-1)-(i1a)).latin1();
-	    QString fpris=line.mid(i2a+1,(i2b-1)-(i2a)).latin1();
-	    QString pris1=line.mid(i3a+1,(i3b-1)-(i3a)).latin1();
-	    QString pris2=line.mid(i4a+1,(i4b-1)-(i4a)).latin1();
-	    QString pris3=line.mid(i5a+1,(i5b-1)-(i5a)).latin1();
-	    QString pris4=line.mid(i6a+1,(i6b-1)-(i6a)).latin1();
-	    QString pris5=line.mid(i7a+1,(i7b-1)-(i7a)).latin1();
-	    if (testflag == TRUE){
-		item = new QListViewItem(listViewResultat,artnr,fpris,pris1,pris2,pris3,pris4,pris5);
-	    }else{
-		qDebug("Laddnings körning!");
-		load=TRUE;
-	    }
-//	    qDebug("load=%d",load);
-//	    qDebug("artnr=%s fpris=%s pris1=%s  pris2=%s  pris3=%s  pris4=%s  pris5=%s",artnr.latin1(),fpris.latin1(),pris1.latin1(),pris2.latin1(), pris3.latin1(),pris4.latin1(),pris5.latin1());
-	}
-            lines += line;
-        }
+void frmLoadPrices::slotDataOnStdout()
+{
+    while (process->canReadLineStdout() ) {
+	QString line = process->readStdout();
+	inrad.append(line);
+	inrad.append("\n");
     }
-   pricefile.close();
-   testflag=FALSE;
+    qDebug("slotDataOnStdout::inrad=%s",inrad.latin1());
+}
+
+void frmLoadPrices::slotDataOnStderr()
+{
+    while (process->canReadLineStderr() ) {
+	QString line = process->readStderr();
+	errorrad.append(line);
+	errorrad.append("\n");
+    }
+}
+
+void frmLoadPrices::slotEndOfProcess()
+{
+    int i;
+    i = -1;
+    i = errorrad.find( QRegExp("Error:"), 0 );
+         if (i != -1) {
+	QMessageBox::critical( this, "CHGPRISW",
+		"ERROR!\n"+errorrad
+	);
+	errorrad="";
+	i = -1;
+    }else{
+//     qDebug("slotEndOfProcess()::inrad=%s",inrad.latin1());
+//     qDebug("slotEndOfProcess()::errorrad=%s",errorrad.latin1());
+	QMessageBox::information( this, "CHGPRISW",
+		"Prisuppdatering Klar!\n"
+	);
+	lineEditFilname->clear();
+	inrad="";
+	pushButtonCancel->setText("Sluta");
+     }
 }
 
 void frmLoadPrices::pushButtonHelp_clicked()
@@ -332,9 +381,3 @@ void frmLoadPrices::readResursFil()
     }
     f1.close();
 }
-
-
-
-
-
-
