a486dccd4d4ba09306af0bad2f856ffe2d7c0bd2
janpihlgren <janpihlgren>
2006-02-02 14:01:11 +0000

    Prislistor,organisationsnr,Hjälpknapp

--- prototype/src/DspKund/frmdspkund.ui.h
+++ prototype/src/DspKund/frmdspkund.ui.h
@@ -9,10 +9,10 @@
 /***************************************************************************
                           DSPKUW  -  description
                              -------------------
-		     version 0.2
-    begin                : Tors 2 okt 2003
-    Modified	: Mån 20 okt 2003
-    copyright            : (C) 2003 by Jan Pihlgren
+		     version 0.3
+    begin                : Tors 2 okt  2003
+    Modified	         : Tors 2 febr 2006
+    copyright         : (C) 2003 by Jan Pihlgren
     email                : jan@pihlgren.se
  ***************************************************************************/
 /*****************************************************************
@@ -38,8 +38,10 @@
     QString inrad;
     QString* rad;
     QString errorrad;
+    QString hjelpfil;
 
     QString kundid;
+    QString kundorgnr;
     QString kundnamn;
     QString kundadress;
     QString postnr;
@@ -80,7 +82,8 @@
     QString skuld;
     QString kravdatum;
     QString orderstock;
-    
+    QString prislista;
+
 void frmDspKund::init()
 {
     lineEditKundNr->setFocus();
@@ -90,7 +93,7 @@ void frmDspKund::lineEditKundNr_returnPressed()
 {
     kundid=lineEditKundNr->text();
     if (kundid == "" ){
-    	    QMessageBox::warning( this, "ADDKUW",
+    	    QMessageBox::warning( this, "DSPKUW",
                       "KundID måste fyllas i! \n" );
 	    lineEditKundNr->setFocus();
     }else{
@@ -183,9 +186,10 @@ void frmDspKund::slotGetEndOfProcess()
 	 int i38 = inrad.find( QRegExp("38:"), 0 );
 	 int i39 = inrad.find( QRegExp("39:"), 0 );
 	 int i40 = inrad.find( QRegExp("40:"), 0 );
-	 int i41 = inrad.find( QRegExp("41:"), 0 );	 
-	 int i42 = inrad.length();
-		 
+	 int i41 = inrad.find( QRegExp("41:"), 0 );	/* Orderstock */
+	 int i42 = inrad.find( QRegExp("42:"), 0 );	/* 20060202 prislista.*/
+	 int i43 = inrad.find( QRegExp("END:"), 0 );	/* 20060201 prislista.*/
+
 	 m=i2-i1;
 	 if (i1 != -1){
 	     kundid=inrad.mid(i1+3,m-4);
@@ -194,7 +198,8 @@ void frmDspKund::slotGetEndOfProcess()
 
  	 m=i3-i2;
 	 if (i2 != -1){
-//	     kundorgnr=inrad.mid(i2+2,m-4);
+	     kundorgnr=inrad.mid(i2+3,m-4);
+	     lineEditKundOrgnr->setText(kundorgnr);
 	 }
 
 	 m=i4-i3;
@@ -214,7 +219,7 @@ void frmDspKund::slotGetEndOfProcess()
 	     postnr=inrad.mid(i5+3,m-4);
 	     lineEditKundPostnr->setText(postnr);
 	 }
-	 
+
 	 m=i7-i6;
 	 if (i6 != -1){
 	     postadr=inrad.mid(i6+3,m-4);
@@ -232,7 +237,7 @@ void frmDspKund::slotGetEndOfProcess()
 	     tfnnr=inrad.mid(i8+3,m-4);
 	     lineEditKundTftnNr->setText(tfnnr);
 	 }
-	 
+
 	 m=i10-i9;
 	 if (i9 != -1){
 	     email=inrad.mid(i9+3,m-4);
@@ -240,11 +245,11 @@ void frmDspKund::slotGetEndOfProcess()
 	 }
 
  	 m=i11-i10;
-	 if (i10 != -1){	     
+	 if (i10 != -1){
 	     faxnr=inrad.mid(i10+3,m-4);
-	     lineEditKundFaxNr->setText(faxnr);	     
+	     lineEditKundFaxNr->setText(faxnr);
 	 }
-	 
+
  	 m=i12-i11;
 	 if (i11 != -1){
 	     erref=inrad.mid(i11+3,m-4);
@@ -256,7 +261,7 @@ void frmDspKund::slotGetEndOfProcess()
 	     erreftfnnr=inrad.mid(i12+3,m-4);
 	     lineEditErRefTfnNr->setText(erreftfnnr);
 	 }
- 
+
  	 m=i14-i13;
 	 if (i13 != -1){
 	     errefemail=inrad.mid(i13+3,m-4);
@@ -280,13 +285,13 @@ void frmDspKund::slotGetEndOfProcess()
 	     valuta=inrad.mid(i16+3,m-4);
 	     lineEditValuta->setText(valuta);
 	 }
-	 
+
 	 m=i18-i17;
 	 if (i17 != -1){
 	     betvillkor=inrad.mid(i17+3,m-4);
 	     lineEditKundBetvilk->setText(betvillkor);
 	 }
-	 
+
 	 m=i19-i18;
 	 if (i18 != -1){
 	     levvillkor=inrad.mid(i18+3,m-4);
@@ -310,80 +315,80 @@ void frmDspKund::slotGetEndOfProcess()
 	    kundkat=inrad.mid(i21+3,m-4);
 	    lineEditKundKategori ->setText(kundkat);
 	 }
- 
+
 	 m=i23-i22;
 	 if (i22 != -1){
 	    levplats=inrad.mid(i22+3,m-4);
 	    lineEditKundStdLevplats ->setText(levplats);
 	 }
-	 
+
 	 m=i24-i23;
 	 if (i23 != -1){
 	     ordererk=inrad.mid(i23+3,m-4);
 	    lineEditOrdererk ->setText(ordererk);
 	 }
-	 
-	 
+
+
 	 m=i25-i24;
 	 if (i24 != -1){
 	     plocklista=inrad.mid(i24+3,m-4);
 	    lineEditPlocklista ->setText(plocklista);
 	 }
-	 
+
 	 m=i26-i25;
 	 if (i25 != -1){
 	     foljesedel=inrad.mid(i25+3,m-4);
 	    lineEditFoljesedel->setText(foljesedel);
 	 }
-	 
+
 	 m=i27-i26;
 	 if (i26 != -1){
 	     kravbrev=inrad.mid(i26+3,m-4);
 	    lineEditKravbrev ->setText(kravbrev);
 	 }
-	 
+
 	 m=i28-i27;
 	 if (i27 != -1){
 	     sprakkod=inrad.mid(i27+3,m-4);
 	    lineEditSprakkod->setText(sprakkod);
 	 }
-	 	 
+
  	 m=i29-i28;
 	 if (i28 != -1){
 	     expavg=inrad.mid(i28+3,m-4);
 	    lineEditExpAvgift->setText(expavg);
 	 }
-	 
+
 	 m=i30-i29;
 	 if (i29 != -1){
 	     fraktavg=inrad.mid(i29+3,m-4);
 	    lineEditFraktAvgift->setText(fraktavg);
 	 }
-	 
+
 	 m=i31-i30;
 	 if (i30 != -1){
 	     kreditlimit=inrad.mid(i30+3,m-4);
 	    lineEditKreditlimit->setText(kreditlimit);
 	 }
-	 
+
 	 m=i32-i31;
 	 if (i31 != -1){
 	     kreditdagar=inrad.mid(i31+3,m-4);
 	    lineEditKreditDagar->setText(kreditdagar);
 	 }
-	 
+
 	 m=i33-i32;
 	 if (i32 != -1){
 	     kreditkod=inrad.mid(i32+3,m-4);
 	    lineEditKreditKod->setText(kreditkod);
 	 }
-	 
+
 	 m=i34-i33;
 	 if (i33 != -1){
 	     exportkod=inrad.mid(i33+3,m-4);
 	    lineEditExportkod->setText(exportkod);
 	 }
-	 
+
  	 m=i35-i34;
 	 if (i34 != -1){
 	    skattekod=inrad.mid(i34+3,m-4);
@@ -401,37 +406,44 @@ void frmDspKund::slotGetEndOfProcess()
 	    drojmalsrenta=inrad.mid(i36+3,m-4);
 	    lineEditDrojRenta->setText(drojmalsrenta);
 	 }
-	 
+
 	 m=i38-i37;
 	 if (i37 != -1){
 	     drofmalsfakt=inrad.mid(i37+3,m-4);
 	    lineEditDrojFaktura->setText(drofmalsfakt);
 	 }
-	 
+
 	 m=i39-i38;
 	 if (i38 != -1){
 	    samfaktura=inrad.mid(i38+3,m-4);
 	    lineEditSamFaktura->setText(samfaktura);
 	 }
-	 
+
 	 m=i40-i39;
 	 if (i39 != -1){
 	    kravdatum=inrad.mid(i39+3,m-4);
 	    lineEditKravDatum->setText(kravdatum);
 	 }
-	 
+
 	 m=i41-i40;
 	 if (i40 != -1){
 	    skuld=inrad.mid(i40+3,m-4);
 	    lineEditSkuld->setText(skuld);
 	 }
-	 
+
 	 m=i42-i41;
 	 if (i41 != -1){
 	    orderstock=inrad.mid(i41+3,m-4);
 	    lineEditOrderstock->setText(orderstock);
 	 }
-	  
+	 
+	 m=i43-i42;				/* 20060202 */
+	 if (i42 != -1){
+	    prislista=inrad.mid(i42+3,m-4);
+//	    qDebug("prislista=|%s|",prislista.latin1());
+	    lineEditKundPrislista->setText(prislista);
+	 }
+
 	 inrad="";
 	errorrad="";
 	inrad="";
@@ -457,5 +469,61 @@ void frmDspKund::slotDataOnStdout()
 	inrad.append("\n");
 //	qDebug("inrad= %s",inrad.latin1());
     }
+}
+
+void frmDspKund::readResursFil()
+{
+    /*****************************************************/
+    /*  Läs in .olfixrc filen här			               */
+    /* Plocka fram var hjälpfilen finns			               */
+    /*****************************************************/
+
+    QStringList lines;
+    QString homepath;
+    homepath=QDir::homeDirPath();
+/*    qDebug("Home Path=%s",homepath.latin1());		*/
+
+    QFile f1( homepath+"/.olfixrc" );
+   if ( f1.open( IO_ReadOnly ) ) {
+        QTextStream stream( &f1 );
+        QString line;
+        int rad = -1;
+        while ( !stream.eof() ) {
+            line = stream.readLine(); /* line of text excluding '\n'	*/
+	rad=line.find( QRegExp("HELPFILE="), 0 );
+	if(rad == 0){
+	    hjelpfil=line;
+/*	    qDebug("hjelpfil=%s",hjelpfil.latin1());		*/
+	    hjelpfil=(hjelpfil.right(hjelpfil.length() - 9));
+/*	    qDebug("hjelpfil=%s",hjelpfil.latin1());		*/
+	}
+            lines += line;
+        }
+    }
+    f1.close();
+}
+
+void frmDspKund::pushButtonHelp_clicked()
+{
+    inrad="";				// töm inputbuffer
+    frmDspKund::readResursFil();		// Hämta path till hjälpfilen
+    int i1 = hjelpfil.find( QRegExp(".html"), 0 );
+    hjelpfil=hjelpfil.left(i1);
+    hjelpfil=hjelpfil+"_KUNDORDER.html";
+    hjelpfil=hjelpfil+"#KUNDER";		// Lägg till position
+
+    process = new QProcess();
+    process->addArgument( "./OLFIXHLP");	// OLFIX funktion
+    process->addArgument(hjelpfil);
+	
+    frmDspKund::connect( process, SIGNAL(readyReadStdout() ),this, SLOT(slotDataOnStdout() ) );
+    frmDspKund::connect( process, SIGNAL(readyReadStderr() ),this, SLOT(slotDataOnStderr() ) );	
+    frmDspKund::connect( process, SIGNAL(processExited() ),this, SLOT(slotEndOfProcess() ) );	    
+ 
+    if ( !process->start() ) {
+	// error handling
+    QMessageBox::warning( this, "Start av CHGKUW ", "Kan inte starta OLFIXHLP!\n" );
+   }
+    lineEditKundNr->setFocus();
 }
 
