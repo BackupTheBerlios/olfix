fa008321403416946453b0bd4e701a09b50b6a08
janpihlgren <janpihlgren>
2007-01-30 19:22:39 +0000

    Hjälpknapp och tabordning

--- prototype/src/AddText/frmaddtxt.ui.h
+++ prototype/src/AddText/frmaddtxt.ui.h
@@ -8,8 +8,9 @@
 /***************************************************************************
                           ADDTXTW  -  description
                              -------------------
-		     version 0.1
-    begin                : Ons 2 april 2003
+		     version 0.2
+    begin                : Ons  2 april 2003
+    modified           :Tis 30  jan   2007
     copyright            : (C) 2003 by Jan Pihlgren
     email                : jan@pihlgren.se
  ***************************************************************************/
@@ -33,13 +34,13 @@
 #define VERSION "Version: 0.1\n 2003-06-03"
 
     QProcess* process;
+    QProcess* proc;
     QString inrad;
     QString errorrad;
     QString* rad;
     QString textnr;
     QString text;
-
-    
+    QString hjelpfil;
     
 void frmAddTxt::init()
 {
@@ -146,4 +147,57 @@ void frmAddTxt::TestText()
 {
     frmAddTxt::slotAddTxt();
 //    qDebug("Text=%s",text.latin1());
+}
+
+void frmAddTxt::slotHelp()
+{
+	inrad="";
+	frmAddTxt::readResursFil();			// Hämta path till hjälpfilen
+	
+	int i1 = hjelpfil.find( QRegExp(".html"), 0 );
+	hjelpfil=hjelpfil.left(i1);
+	hjelpfil=hjelpfil+"_FTGADMIN.html";
+	hjelpfil=hjelpfil+"#NYATEXTER";			// Lägg till position
+/*	qDebug("hjelpfil=%s",hjelpfil.latin1());	*/
+
+	proc = new QProcess();
+	proc->addArgument( "./OLFIXHLP" );			// OLFIX program
+	proc->addArgument(hjelpfil);
+
+	if ( !proc->start() ) {
+	    // error handling
+	    QMessageBox::warning( this, "OLFIX","Kan inte starta OLFIXHLP!\n" );
+	}
+}
+
+void frmAddTxt::readResursFil()
+{
+    /*****************************************************/
+    /*  Läs in .olfixrc filen här			            	*/
+    /* Plocka fram var hjälpfilen finns			*/
+    /*****************************************************/
+
+    QStringList lines;
+    QString homepath;
+    homepath=QDir::homeDirPath();
+/*    qDebug("Home Path=%s",homepath.latin1());		*/
+ 
+    QFile f1( homepath+"/.olfixrc" );
+   if ( f1.open( IO_ReadOnly ) ) {
+        QTextStream stream( &f1 );
+        QString line;
+        int rad = -1;
+        while ( !stream.eof() ) {
+            line = stream.readLine(); /* line of text excluding '\n'	*/
+	rad=line.find( QRegExp("HELPFILE="), 0 ); 
+	if(rad == 0){
+	    hjelpfil=line;
+/*	    qDebug("hjelpfil=%s",hjelpfil.latin1());		*/
+	    hjelpfil=(hjelpfil.right(hjelpfil.length() - 9));
+/*	    qDebug("hjelpfil=%s",hjelpfil.latin1());		*/
+	}
+            lines += line;
+        }
+    }
+    f1.close();
 }
