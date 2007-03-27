f725e2ca8ed4f1e8738dd818889e529b023e2bf9
janpihlgren <janpihlgren>
2007-03-27 04:11:47 +0000

    Check av att user och funktion existerar

--- prototype/src/AddRights/frmaddright.ui.h
+++ prototype/src/AddRights/frmaddright.ui.h
@@ -34,11 +34,14 @@
 #define MAXSTRING 15000
 
 QProcess* process;
+QProcess* proc;
 QString inrad;
 QString errorrad;
 QString* rad;
 QString Userid;
 QString Funk;
+
+QString hjelpfil;
 
 
 void frmAddRight::init()
@@ -62,6 +65,7 @@ void frmAddRight::slotRightEntered()
     Funk=LineEditFunk->text();
     Funk=Funk.upper();
     LineEditFunk->setText((Funk));
+    checkFunction(Funk);
 }
 
 void frmAddRight::slotAddRight()
@@ -248,6 +252,7 @@ void frmAddRight::slotEndOfGetFuncfProcess()
 	//	 rensa listrad
 	listrad.remove(0,70);
     }
+    slotGetProgram();
 }
 
 
@@ -326,6 +331,245 @@ void frmAddRight::checkFunction( QString function )
 	flag=TRUE;
     }
     if(flag==FALSE){
-	qDebug("%s finns inte!",function.latin1());
+//	qDebug("%s finns inte!",function.latin1());
+	QMessageBox::warning( this, "ADDRGTW","Fel! Programmet/funktionen "+function+
+			       " finns inte!\n"
+			       );
+	LineEditFunk->setFocus();
+	LineEditFunk->selectAll();
+	Funk="";
+	return;
+    }
+}
+
+void frmAddRight::slotHelp()
+{
+	inrad="";
+	readResursFil();			// Hämta path till hjälpfilen
+	
+	int i1 = hjelpfil.find( QRegExp(".html"), 0 );
+	hjelpfil=hjelpfil.left(i1);
+	hjelpfil=hjelpfil+"_ADMINISTRATION.html";
+	hjelpfil=hjelpfil+"#NYBEHORIG";			// Lägg till position
+/*	qDebug("hjelpfil=%s",hjelpfil.latin1());	*/
+
+	proc = new QProcess();
+	proc->addArgument( "./OLFIXHLP" );			// OLFIX program
+	proc->addArgument(hjelpfil);
+
+	if ( !proc->start() ) {
+	    // error handling
+	    QMessageBox::warning( this, "OLFIX","Kan inte starta OLFIXHLP!\n" );
+	}
+}
+
+void frmAddRight::readResursFil()
+{
+    /*****************************************************/
+    /*  Läs in .olfixrc filen här			               */
+    /* Plocka fram var hjälpfilen finns			               */
+    /*****************************************************/
+
+    QStringList lines;
+    QString homepath;
+    homepath=QDir::homeDirPath();
+/*    qDebug("Home Path=%s",homepath.latin1());		*/
+
+    QFile f1( homepath+"/.olfixrc" );
+   if ( f1.open( IO_ReadOnly ) ) {
+        QTextStream stream( &f1 );
+        QString line;
+        int rad = -1;
+        while ( !stream.eof() ) {
+            line = stream.readLine(); /* line of text excluding '\n'	*/
+	rad=line.find( QRegExp("HELPFILE="), 0 );
+	if(rad == 0){
+	    hjelpfil=line;
+/*	    qDebug("hjelpfil=%s",hjelpfil.latin1());		*/
+	    hjelpfil=(hjelpfil.right(hjelpfil.length() - 9));
+/*	    qDebug("hjelpfil=%s",hjelpfil.latin1());		*/
+	}
+            lines += line;
+        }
+    }
+    f1.close();
+}
+
+void frmAddRight::slotGetProgram()
+{
+	const char *userp = getenv("USER");
+                QString usr(userp);
+	inrad="";
+	errorrad="";
+
+	process = new QProcess();
+	process->addArgument("./STYRMAN");	// OLFIX huvudprogram
+	process->addArgument(usr);		// userid
+	process->addArgument( "PRGLST");	// OLFIX funktion
+
+	frmAddRight::connect( process, SIGNAL(readyReadStdout() ),this, SLOT(slotDataOnStdout() ) );
+	frmAddRight::connect( process, SIGNAL(readyReadStderr() ),this, SLOT(slotDataOnStderr() ) );
+	frmAddRight::connect( process, SIGNAL(processExited() ),this, SLOT(slotPgmEndOfProcess() ) );
+	
+	if ( !process->start() ) {
+                // error handling
+	    QMessageBox::warning( this, "Start av PRGLST ",
+                            "Kan inte starta STYRMAN/PRGLST!\n"
+                            );
+        }
+}
+
+void frmAddRight::slotPgmEndOfProcess()
+{
+//    QListViewItem* item;
+    int i;
+    i = -1;
+    i = errorrad.find( QRegExp("Error:"), 0 );
+         if (i != -1) {
+	QMessageBox::critical( this, "OLFIXW",
+		"ERROR!\n"+errorrad
+	);
+	errorrad="";
+	i = -1;
+     }
+	 
+    i = -1;
+    i = errorrad.find( QRegExp("error"), 0 );
+         if (i != -1) {
+	QMessageBox::critical( this, "OLFIXW",
+		"ERROR!\n"+errorrad
+	);
+	errorrad="";
+	i = -1;
+     }	 
+	 
+    i = -1;
+    i = inrad.find( QRegExp("Error:"), 0 );
+         if (i != -1) {
+	QMessageBox::critical( this, "OLFIXW",
+		"ERROR!\n"+inrad
+	);
+	inrad="";
+	i = -1;
+     }
+    QListViewItem* item;
+    QString listrad;
+    rad=&inrad;
+    inrad.latin1();
+    char *pos1;
+    char *pos2;
+    char tmp[MAXSTRING];
+    char *tmppek;
+    int j,k,l,m;
+    int pgmfoldnr,mnufoldnr,mnutxtnr;
+    
+    char antrad[6]="";
+    char prgnr[4]="";
+    char proggrp[21]="";
+//    char prggrptemp[21]="";
+    char mnugrp[31]="";
+//    char mnugrptemp[31]="";
+    char mnutxt[31]="";
+//    char mnutxttemp[31]="";
+    char pgm[9]="";
+
+    pgmfoldnr=0;
+    mnufoldnr=0;
+    mnutxtnr=0;
+    tmppek=tmp;
+    qstrcpy(tmp,inrad);
+    pos1=strstr(tmp,"NR_");
+    pos2=strstr(tmp,"_:");
+    i=pos2-pos1;
+    m=i+2+3;		// startposition
+//    fprintf(stdout,"i=%d  m=%d",i,m);
+    k=0;
+    for (j=3;j<i;j++){
+	antrad[k]=tmp[j];
+	k++;
+    };
+    i=atoi(antrad);		// i = antal poster
+//    fprintf(stderr,"Antalrader=%d\n",i);
+    
+    for (k = 1;k <= i; k++){	// gå igenom alla raderna / posterna
+	l=0;
+	for(j = m; j < sizeof(prgnr) + m; j++){
+	    if(tmp[j] != *("_")){
+		prgnr[l]=tmp[j];
+		l++;
+	    }else{
+		prgnr[l] = *("\0");
+		j=sizeof(prgnr) + m;
+	    }
+	}
+//	fprintf(stdout,"%s  ",prgnr);
+	m=m+l+2+3;	// position för proggrp
+	l=0;
+	for(j = m; j < sizeof(proggrp) + m; j++){
+	    if(tmp[j] != *("_")){
+		proggrp[l]=tmp[j];
+		l++;
+	    }else{
+		proggrp[l] = *("\0");
+		j=sizeof(proggrp) + m;
+	    }
+	}
+//	fprintf(stdout,"%s  ",proggrp);
+	m=m+l+2+3;	// position för mnugrp
+	l=0;
+	for(j = m; j < sizeof(mnugrp) + m; j++){
+	    if(tmp[j] != *("_")){
+		mnugrp[l]=tmp[j];
+		l++;
+	    }else{
+		mnugrp[l] = *("\0");
+		j=sizeof(mnugrp) + m;
+	    }
+	}
+//	fprintf(stdout,"%s  ",mnugrp);
+	m=m+l+2+3;	// position för mnutxt
+	l=0;
+	for(j = m; j < sizeof(mnutxt) + m; j++){
+	    if(tmp[j] != *("_")){
+		mnutxt[l]=tmp[j];
+		l++;
+	    }else{
+		mnutxt[l] = *("\0");
+		j=sizeof(mnutxt) + m;
+	    }
+	}
+//	fprintf(stdout,"%s\n  ",mnutxt);
+	m=m+l+2+3;	//position för pgm	
+	l=0;
+	for(j = m; j < sizeof(pgm) + m; j++){
+	    if(tmp[j] != *("_")){
+		pgm[l]=tmp[j];
+		l++;
+	    }else{
+		pgm[l] = *("\0");
+		j=sizeof(pgm) + m;
+	    }
+	}
+	m=m+l+2+3;
+// 	fprintf(stdout,"%s\n  ",mnutxt);	
+//	qDebug("%s %s %s %s\n",proggrp,mnugrp,mnutxt,pgm);
+	QString prog;
+	prog=pgm;
+	prog=prog.stripWhiteSpace();
+	if(prog !=""){
+	    item = new QListViewItem(ListViewBehor_2,pgm,mnutxt);
+	}
+	
+	for (l=0;l<sizeof(prgnr);l++)
+		prgnr[l]=*("\0");
+	for (l=0;l<sizeof(proggrp);l++)
+		proggrp[l]=*("\0");
+	for (l=0;l<sizeof(mnugrp);l++)
+		mnugrp[l]=*("\0");
+	for (l=0;l<sizeof(mnutxt);l++)
+		mnutxt[l]=*("\0");
+	for (l=0;l<sizeof(pgm);l++)
+		pgm[l]=*("\0");
+	listrad.remove(0,70);
     }
 }
