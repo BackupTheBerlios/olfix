89a70ad1793aa03ba599f331a82bbca5106377ec
janpihlgren <janpihlgren>
2007-05-01 13:23:09 +0000

    Lagt till hjälpknapp

--- prototype/src/AddLevsett/frmaddlevsett.ui.h
+++ prototype/src/AddLevsett/frmaddlevsett.ui.h
@@ -8,8 +8,9 @@
 /***************************************************************************
                           ADDLEVSW  -  description
                              ---------------
-    version	: 0.1
-    begin                   : Mån 9 febr 2004
+    version	: 0.2
+    begin                   : Mån 9 febr   2004
+    modified	            : Lör 28 april 2007
     copyright            : (C) 2003 by Jan Pihlgren
     email                : jan@pihlgren.se
  ***************************************************************************/
@@ -37,6 +38,7 @@
     QString levsett;
     QString dagar;
     QString beskrivning;
+    QString hjelpfil;
     
     
 void frmAddLevsett::init()
@@ -137,4 +139,59 @@ void frmAddLevsett::slotEndOfProcess()
 	inrad="";
 	i = -1;
      }
+}
+
+void frmAddLevsett::PushButtonHelp_clicked()
+{
+	inrad="";
+	frmAddLevsett::readResursFil();		// Hämta path till hjälpfilen
+	
+	int i1 = hjelpfil.find( QRegExp(".html"), 0 );
+//	int i2 = hjelpfil.length();
+	hjelpfil=hjelpfil.left(i1);
+	hjelpfil=hjelpfil+"_ADMINISTRATION.html";
+	hjelpfil=hjelpfil+"#NYLEVSETT";		// Lägg till position
+//	qDebug("hjelpfil=%s",hjelpfil.latin1());
+
+	process = new QProcess();
+	process->addArgument( "./OLFIXHLP" );		// OLFIX program
+	process->addArgument(hjelpfil);
+
+	if ( !process->start() ) {
+	    // error handling
+	    QMessageBox::warning( this, "OLFIX","Kan inte starta OLFIXHLP!\n" );
+	}
+	LineEditLevsett->setFocus();
+}
+
+void frmAddLevsett::readResursFil()
+{
+    /*****************************************************/
+    /*  Läs in .olfixrc filen här			               */
+    /* Plocka fram var hjälpfilen finns			               */
+    /*****************************************************/
+
+    QStringList lines;
+    QString homepath;
+    homepath=QDir::homeDirPath();
+/*    qDebug("Home Path=%s",homepath.latin1());		*/
+
+    QFile f1( homepath+"/.olfixrc" );
+   if ( f1.open( IO_ReadOnly ) ) {
+        QTextStream stream( &f1 );
+        QString line;
+        int rad = -1;
+        while ( !stream.eof() ) {
+            line = stream.readLine(); /* line of text excluding '\n'	*/
+	rad=line.find( QRegExp("HELPFILE="), 0 );
+	if(rad == 0){
+	    hjelpfil=line;
+/*	    qDebug("hjelpfil=%s",hjelpfil.latin1());		*/
+	    hjelpfil=(hjelpfil.right(hjelpfil.length() - 9));
+/*	    qDebug("hjelpfil=%s",hjelpfil.latin1());		*/
+	}
+            lines += line;
+        }
+    }
+    f1.close();
 }
