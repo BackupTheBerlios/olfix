1656bb0458f513973747efd75b97754c2ea8646c
janpihlgren <janpihlgren>
2007-12-08 07:34:16 +0000

    Inför val av databashost

--- prototype/src/VALDSP.c
+++ prototype/src/VALDSP.c
@@ -1,11 +1,12 @@
 /***************************************************************************
                           VALDSP.c  -  description
                              -------------------
-    Version		 : 0.4
+    Version		 : 0.5
     begin                : Fre   21 febr 2003
-    modified		 : Mån   19 febr 2007
+    modified		 : Fre    7 nov  2007
     copyright            : (C) 2002 by Jan Pihlgren
     email                : jan@pihlgren.se
+	Anrop till databas pÃ¥ annan server/host
  ***************************************************************************/
 
 /***************************************************************************
@@ -22,14 +23,14 @@
 
 	Kommando: ./VALDSP valutaid
 
-	Function: Visa all information för valutaid i tabell VALUTA
+	Function: Visa all information fï¿½r valutaid i tabell VALUTA
 
 	OUTPUT: VALUTAID, LAND, BETECKNING, SALJ, KOP samt errornb och error (text)
 
 */
 
  /*@unused@*/ static char RCS_id[] =
-    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/VALDSP.c,v 1.4 2007/02/19 07:03:19 janpihlgren Exp $ " ;
+    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/VALDSP.c,v 1.5 2007/12/08 07:34:16 janpihlgren Exp $ " ;
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -46,30 +47,42 @@
   MYSQL_ROW sqlrow;
 
 int which_database(char *envp[]);
+int which_host(char *envp[]);	// 20071207
 char database[15]="";
+char host[200]="";		// 20071207
 
 int main(int argc, char *argv[], char *envp[])
 {
-/* int i;	*/
+
   int res;
   int status;
-  const char *userp = getenv("USER");	/* vem är inloggad?	*/
+  const char *userp = getenv("USER");	/* vem Ã¤r inloggad?	*/
   char databas[25]="olfix";
-  char usr[21];				/* userid 20070219 utökat från 15 till 21 tecken */
+  char usr[21];				/* userid 20070219 utÃ¶kat frÃ¥n 15 till 21 tecken */
   int num_rows;
 
   char temp1[]="SELECT VALUTAID, LAND, BETECKNING, KOP, SALJ FROM VALUTA WHERE VALUTAID = \"";
   char temp2[]="\"";
   char temp5[200]="";
   char valuta[4]="";
-
-/*  for (i=0;i< argc;i++){
-  	fprintf(stderr,"VALDSPmain argv%d = %s\n",i,argv[i]);
+/*
+  int i;
+  for (i=0;i< argc;i++){
+  	fprintf(stderr,"VALDSPmain argc=%d argv%d = %s\n",i,argc,argv[i]);
   }
 */
+  if (argc < 2){
+	fprintf(stderr,"Error: Valuta saknas!\n");
+	exit(0);
+  }
 /* ================================================================================ */
 /* 		Val av databas, START						    */
 /* ================================================================================ */
+/*	fprintf(stderr,"1. host=%s\n",host);	*/
+  status = which_host(envp);			// 20071207
+	fprintf(stdout,"host=%s ",host);	/* Det ska vara ett mellanslag i slutet! */
+  if (status != 0)
+	exit(status);
 
   status = which_database(envp);
 
@@ -82,7 +95,7 @@ int main(int argc, char *argv[], char *envp[])
     	if (strlen(database)!= 0){
 		strncpy(databas,database,sizeof(databas));		/* 2005-02-24	*/
 	}else{
-  		strncpy(databas,"olfixtst",15);	/* olfixtst = testföretag	*/
+  		strncpy(databas,"olfixtst",15);	/* olfixtst = testfï¿½retag	*/
 	}
   }else{
 	if (strlen(argv[ANTARG]) != 0){
@@ -94,7 +107,7 @@ int main(int argc, char *argv[], char *envp[])
   	}
   }
 /*  fprintf(stderr,"ANTARG=%d,argv[ANTARG]=%s\n",ANTARG,argv[ANTARG]);	*/
-/* Om usr (userid) börjar på 'test' eller 'prov' använd databas 'olfixtst' */
+/* Om usr (userid) bÃ¶rjar pÃ¥ 'test' eller 'prov' anvÃ¤nd databas 'olfixtst' */
   if (strncmp(usr,"test",4)==0 || strncmp(usr,"prov",4)==0 ) {
   	strncpy(databas,"olfixtst",15);
   }
@@ -115,7 +128,7 @@ int main(int argc, char *argv[], char *envp[])
 /*  fprintf(stderr,"VALDSPmain: temp5 = %s\n",temp5);	*/
 
   mysql_init(&my_connection);
-  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", databas, 0, NULL, 0)){
+  if (mysql_real_connect(&my_connection, host,  "olfix", "olfix", databas, 0, NULL, 0)){
 /*  fprintf(stdout,"RGTDSPmain:Connection success\n");		*/
 
   res = mysql_query(&my_connection,temp5);
@@ -202,7 +215,7 @@ int which_database(char *envp[])
 	}
 	else{
 /*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
-	 	fprintf(stderr,"Error: Filen .olfixrc kan inte öppnas\n");
+	 	fprintf(stderr,"Error: Filen .olfixrc kan inte ï¿½ppnas\n");
 	}
 	for (i=0;i < strlen(database);i++){
 		tmp[i]=database[i];
@@ -212,6 +225,70 @@ int which_database(char *envp[])
 	strncpy(database,tmp,strlen(tmp));
 	database[strlen(tmp)]=0;
 /*	fprintf(stderr,"databas=%s\n",database);	*/
+
+	return status;
+}
+
+int which_host(char *envp[])
+{
+	FILE *fil_pek;
+
+	char home[50];
+	char *home_pek;
+	char resource[]="/.olfixrc";
+	char filename[50]="";
+	char tmp[20]="";
+	char temp[10]="";
+	char *tmp_pek;
+	int i,status;
+	
+	strncpy(host,"localhost",10);		/* default, Ã¶verskrivs om .olfirc innehÃ¥ller en hostadr */
+
+	for (i = 0;envp[i]!=NULL;i++){
+		if(strstr(envp[i],"HOME=") != NULL){
+			strncpy(temp,envp[i],4);
+/*			fprintf(stderr,"temp=%s\n",temp); */
+			status=strcmp(temp,"HOME");
+/*			fprintf(stderr,"status=%d\n",status); */
+			if (status == 0){
+				home_pek=(strstr(envp[i],"HOME="));
+				home_pek=home_pek+5;
+				strcpy(home,home_pek);
+			}
+/*			fprintf(stderr,"home_pek=%d %s\n",home_pek,home_pek);	*/
+		}
+	}
+/*	fprintf(stderr,"home=%s\n",home);	*/
+	strncpy(filename,home,strlen(home));
+	strncat(filename,resource,strlen(resource));
+
+/*	fprintf(stderr,"filename=%s\n",filename);	*/
+	status=-1;
+
+	if ((fil_pek = fopen(filename,"r")) != NULL){
+		while (fgets(tmp,150,fil_pek) != NULL){
+/*			fprintf(stderr,"tmp=%s\n",tmp); 	*/
+			if(strstr(tmp,"HOST=")){
+				tmp_pek=(strstr(tmp,"HOST="))+5;
+				strncpy(host,tmp_pek,strlen(tmp_pek));
+				status=0;
+			}
+		}
+/*		fprintf(stderr," Host=%s_len=%d\n",host,strlen(host)); 	*/
+		fclose(fil_pek);
+	}
+	else{
+/*		fprintf(stderr," Host=%s_len=%d\n",host,strlen(host)); 	*/
+	 	fprintf(stderr,"Error: Filen .olfixrc kan inte Ã¶ppnas\n");
+	}
+	for (i=0;i < strlen(host);i++){
+		tmp[i]=host[i];
+	}
+	tmp[i-1]=0;
+/*	fprintf(stderr,"tmp=%s, i=%d len(tmp)=%d\n",tmp,i,strlen(tmp));	*/
+	strncpy(host,tmp,strlen(tmp));
+	host[strlen(tmp)]=0;
+/*	fprintf(stderr,"host=%s\n",host);	*/
 
 	return status;
 }
