63e85053ddb9bdb92ab4f3d4ece8994f07e01dd6
janpihlgren <janpihlgren>
2007-12-10 06:38:13 +0000

    Inför val av databashost

--- prototype/src/DelValuta/frmdelvaluta.ui.h
+++ prototype/src/DelValuta/frmdelvaluta.ui.h
@@ -8,10 +8,11 @@
 /***************************************************************************
                           DELVALW  -  description
                              -------------------
-		     version 0.01
-    begin                : Fre 21 febr 2003
-    copyright            : (C) 2003 by Jan Pihlgren
-    email                : jan@pihlgren.se
+		     version 0.1
+    begin                 : Fre   21 febr 2003
+    modified           : Mån 10 dec  2007
+    copyright         : (C) 2003 by Jan Pihlgren
+    email                 : jan@pihlgren.se
  ***************************************************************************/
 /*****************************************************************
  *					                                                 *
@@ -39,9 +40,11 @@
     QString land;
     QString kop;
     QString salj;
+    QString hjelpfil;
 
 void frmDelValuta::init()
 {
+    listValuta();			/* 20071210  */
     LineEditValuta->setFocus();
 }
 
@@ -55,8 +58,9 @@ void frmDelValuta::slotPushButtonGet_clicked()
 void frmDelValuta::slotGetValuta()
 {
 	const char *userp = getenv("USER");
-            QString usr(userp);
-
+                QString usr(userp);
+	inrad="";
+	
 	 LineEditValuta->clear();
 	 LineEditBeteck->clear();
 	 LineEditLand->clear();
@@ -85,6 +89,29 @@ void frmDelValuta::slotGetValuta()
                             "Kan inte starta STYRMAN/VALDSP! \n" );
 	    }   
 	}
+}
+
+void frmDelValuta::listValuta()
+{
+	const char *userp = getenv("USER");
+	QString usr(userp);
+	
+	inrad="";
+	process = new QProcess();
+	process->addArgument("./STYRMAN");	// OLFIX styrprogram
+	process->addArgument(usr);		// valutaid
+	process->addArgument( "VALLST");	// OLFIX funktion
+
+	frmDelValuta::connect( process, SIGNAL(readyReadStdout() ),this, SLOT(slotGetDataOnStdout() ) );
+	frmDelValuta::connect( process, SIGNAL(readyReadStderr() ),this, SLOT(slotGetDataOnStderr() ) );
+                frmDelValuta::connect( process, SIGNAL(processExited() ),this, SLOT(slotListaEndOfProcess() ) );
+
+	if ( !process->start() ) {
+		// error handling
+		fprintf(stderr,"Problem starta STYRMAN/VALLST!\n");
+		QMessageBox::warning( this, "DELVALW",
+                            "Kan inte starta STYRMAN/VALLST! \n" );
+	    }   	
 }
 
 void frmDelValuta::slotValutaEntered()
@@ -142,26 +169,31 @@ void frmDelValuta::slotEndOfGetProcess()
     m=j-i;
     if (i != -1){
 	valuta=inrad.mid(i+2,m-2);
+	valuta = valuta.stripWhiteSpace(); 
 	LineEditValuta->setText(valuta);
     }
     m=k-j;
     if ( j != -1){
 	land=inrad.mid(j+2,m-2);
+	land = land.stripWhiteSpace();	
 	LineEditLand->setText(land);
     }
     m=l-k;
     if ( k != -1){
 	beteckning=inrad.mid(k+2,m-2);
+	beteckning = beteckning.stripWhiteSpace();
 	LineEditBeteck->setText(beteckning);
     }
      m=n-l;
     if ( l != -1){
 	kop=inrad.mid(l+2,m-2);
+	kop = kop.stripWhiteSpace();
 	LineEditKop->setText(kop);
     }
     m=inrad.length() - n;
     if ( n != -1){
 	salj=inrad.mid(n+2,m-3);
+	salj = salj.stripWhiteSpace();
 	LineEditSalj->setText(salj);
     } 
         LineEditValuta->setFocus();
@@ -250,3 +282,211 @@ void frmDelValuta::slotDelDataOnStdout()
 	inrad.append("\n");
     }
 }
+
+void frmDelValuta::slotListaEndOfProcess()
+{
+       QListViewItem* item;
+    int i;
+    
+    i = -1;
+    i = errorrad.find( QRegExp("Error:"), 0 );
+         if (i != -1) {
+	QMessageBox::critical( this, "DELVALW",
+		"ERROR!\n"+errorrad
+	);
+	errorrad="";
+	i = -1;
+     }
+    QString listrad;
+    rad=&inrad;
+    inrad.latin1();
+    char *pos1;
+    char *pos2;
+    char tmp[MAXSTRING];
+    char *tmppek;
+    int j,k,l,m,m1,m2,l1,l2;
+    char antrad[6]="";
+    char valuta[4]="";
+    char beteckning[16]="";
+    char kop[8]="";
+    char salj[8]="";
+    char land[16]="";
+    QString host="";
+    /*                                                   Start     2007-12-07                                         */
+    m1=inrad.find( QRegExp("host="), 0 );
+    m2=inrad.find( QRegExp("NR_"), 0 );
+    l1=m2-(m1+5);
+    l2=m2-m1;
+    host=inrad.mid(5,l1);
+    inrad=inrad.mid(m2,inrad.length()-m2);
+    
+//  qDebug("host=%s m1=%d m2=%d l1=%d l2=%d\n",host.latin1(),m1,m2,l1,l2);
+    if(host != "127.0.0.1 "){
+	 if(host != "localhost "){
+	     textLabel1->setText("<u><b>Host</b></u>\n");
+	     textLabelHostName->setText(host);
+	 }
+    }else{
+	textLabel1->setText("");
+    }
+    /*                                                End         2007-12-07                                         */
+    
+    tmppek=tmp;
+    qstrcpy(tmp,inrad);
+    pos1=strstr(tmp,"NR_");
+    pos2=strstr(tmp,"_:");
+    i=pos2-pos1;
+    m=i+2;		// startposition för första valutaid.
+//    fprintf(stdout,"i=%d  m=%d",i,m);
+    k=0;
+    for (j=3;j<i;j++){
+	antrad[k]=tmp[j];
+	k++;
+    };
+    i=atoi(antrad);		// i = antal poster
+    for (k = 1;k <= i; k++){	// gå igenom alla raderna / posterna
+	l=0;
+	for(j = m; j < sizeof(valuta) + m; j++){
+	    if(tmp[j] != *("_")){
+		valuta[l]=tmp[j];
+		l++;
+	    }else{
+		valuta[l] = *("\0");
+		j=sizeof(valuta) + m;
+	    }
+	}
+//	fprintf(stdout,"%s  ",valuta);
+	m=m+l+2;	// position för land
+	l=0;
+	for(j = m; j < sizeof(land) + m; j++){
+	    if(tmp[j] != *("_")){
+		land[l]=tmp[j];
+		l++;
+	    }else{
+		land[l] = *("\0");
+		j=sizeof(land) + m;
+	    }
+	}
+//	fprintf(stdout,"%s  ",land);
+	m=m+l+2;	// position för kop
+	l=0;
+	for(j = m; j < sizeof(kop) + m; j++){
+	    if(tmp[j] != *("_")){
+		kop[l]=tmp[j];
+		l++;
+	    }else{
+		kop[l] = *("\0");
+		j=sizeof(kop) + m;
+	    }
+	}
+//	fprintf(stdout,"%s  ",kop);
+	m=m+l+2;	// position för salj
+	l=0;
+	for(j = m; j < sizeof(salj) + m; j++){
+	    if(tmp[j] != *("_")){
+		salj[l]=tmp[j];
+		l++;
+	    }else{
+		salj[l] = *("\0");
+		j=sizeof(salj) + m;
+	    }
+	}
+//	fprintf(stdout,"%s\n  ",salj);			
+	m=m+l+2;	// position för beteckning
+	l=0;
+	for(j = m; j < sizeof(beteckning) + m; j++){
+	    if(tmp[j] != *("_")){
+		beteckning[l]=tmp[j];
+		l++;
+	    }else{
+		beteckning[l] = *("\0");
+		j=sizeof(beteckning) + m;
+	    }
+	}
+//	fprintf(stdout,"%s\n  ",beteckning);	
+	m=m+l+2;
+		
+	item = new QListViewItem(listViewValuta,valuta,beteckning);
+// 	 rensa valuta,beteckning,kop och salj 
+  	for (l=0;l<sizeof(valuta);l++)
+		valuta[l]=*("\0");
+	for (l=0;l<sizeof(beteckning);l++)
+		beteckning[l]=*("\0");
+	listrad.remove(0,70);
+    }   
+}
+
+void frmDelValuta::slotPickupValuta( QListViewItem * item)
+{
+    char val[4]="";
+//    qDebug("PickupValuta\n");
+    if(!item){
+	return;
+    }
+     listViewValuta->setCurrentItem(item);
+     if(!item->key(0,TRUE)){
+	 return;
+     }
+
+     strcpy(val,item->key(0,TRUE));	// = Valuta
+     valuta=val;
+     LineEditValuta->setText(valuta);
+     LineEditValuta->setFocus();
+}
+
+void frmDelValuta::pushBtnHelp_clicked()
+{
+	inrad="";
+	frmDelValuta::readResursFil();		// Hämta path till hjälpfilen
+
+	int i1 = hjelpfil.find( QRegExp(".html"), 0 );
+//	int i2 = hjelpfil.length();
+	hjelpfil=hjelpfil.left(i1);
+	hjelpfil=hjelpfil+"_EKONOMI.html";
+	hjelpfil=hjelpfil+"#DELVALUTA";		// Lägg till position
+//	qDebug("hjelpfil=%s",hjelpfil.latin1());
+
+	process = new QProcess();
+	process->addArgument( "./OLFIXHLP" );	// OLFIX program
+	process->addArgument(hjelpfil);
+
+	if ( !process->start() ) {
+	    // error handling
+	    QMessageBox::warning( this, "OLFIX","Kan inte starta OLFIXHLP!\n" );
+	}
+	LineEditValuta->setFocus();
+}
+
+void frmDelValuta::readResursFil()
+{
+    /*****************************************************/
+    /*  Läs in .olfixrc filen h�r			               	*/
+    /* Plocka fram var hjälpfilen finns			*/
+    /*****************************************************/
+
+    QStringList lines;
+    QString homepath;
+    homepath=QDir::homeDirPath();
+/*    qDebug("Home Path=%s",homepath.latin1());		*/
+
+    QFile f1( homepath+"/.olfixrc" );
+   if ( f1.open( IO_ReadOnly ) ) {
+        QTextStream stream( &f1 );
+        QString line;
+        int rad = -1;
+        while ( !stream.eof() ) {
+            line = stream.readLine(); /* line of text excluding '\n'	*/
+	rad=line.find( QRegExp("HELPFILE="), 0 );
+	if(rad == 0){
+	    hjelpfil=line;
+/*	    qDebug("hjelpfil=%s",hjelpfil.latin1());		*/
+	    hjelpfil=(hjelpfil.right(hjelpfil.length() - 9));
+/*	    qDebug("hjelpfil=%s",hjelpfil.latin1());		*/
+	}
+            lines += line;
+        }
+    }
+    f1.close();
+}
+
+
--- prototype/src/VALDEL.c
+++ prototype/src/VALDEL.c
@@ -1,11 +1,12 @@
 /***************************************************************************
                           VARDEL.c  -  description
                              -------------------
-    Version		 : 0.3
+    Version		 : 0.4
     begin                : Fre  21 febr	2003
-    modified		 : Tors 24 febr 2005
+    modified		 : M√•n  10 dec  2007
     copyright            : (C) 2003 by Jan Pihlgren
     email                : jan@pihlgren.se
+	Anrop till databas p√• annan server/host
  ***************************************************************************/
 
 /***************************************************************************
@@ -28,7 +29,7 @@
 
 */
  /*@unused@*/ static char RCS_id[] =
-    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/VALDEL.c,v 1.4 2007/02/19 07:03:19 janpihlgren Exp $ " ;
+    "@(#) $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/olfix/Repository/prototype/src/VALDEL.c,v 1.5 2007/12/10 06:38:13 janpihlgren Exp $ " ;
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -44,15 +45,17 @@
   MYSQL_RES *res_ptr;
   MYSQL_ROW sqlrow;
 
-int which_database(char *envp[]);
-char database[15]="";
+  int which_database(char *envp[]);
+  int which_host(char *envp[]);	// 20071210
+  char host[200]="";		// 20071210
+  char database[15]="";
 
 int main(int argc, char *argv[], char *envp[])
 {
 /* int i;	*/
   int res;
   int status;
-  const char *userp = getenv("USER");	/* vem ‰r inloggad?	*/
+  const char *userp = getenv("USER");	/* vem √§r inloggad?	*/
   char databas[25]="olfix";
   char usr[15];				/* userid		*/
 
@@ -61,9 +64,21 @@ int main(int argc, char *argv[], char *envp[])
   char temp5[200]="";
   char valuta[4]="";
 
+/* Start 2007-12-10	*/
+  if (argc < 2){
+	fprintf(stderr,"Error: Valuta saknas!\n");
+	exit(0);
+  }
+/* End 2007-12-10	*/
+
 /* ================================================================================ */
 /* 		Val av databas, START						    */
 /* ================================================================================ */
+/*	fprintf(stderr,"1. host=%s\n",host);	*/
+  status = which_host(envp);			// 20071210
+	fprintf(stdout,"host=%s ",host);	/* Det ska vara ett mellanslag i slutet! */
+  if (status != 0)
+	exit(status);
 
   status = which_database(envp);
 
@@ -76,7 +91,7 @@ int main(int argc, char *argv[], char *envp[])
     	if (strlen(database)!= 0){
 		strncpy(databas,database,sizeof(databas));		/* 2005-02-24	*/
 	}else{
-  		strncpy(databas,"olfixtst",15);	/* olfixtst = testfˆretag	*/
+  		strncpy(databas,"olfixtst",15);	/* olfixtst = testf√∂retag	*/
 	}
   }else{
 	if (strlen(argv[ANTARG]) != 0){
@@ -88,7 +103,7 @@ int main(int argc, char *argv[], char *envp[])
   	}
   }
 /*  fprintf(stderr,"ANTARG=%d,argv[ANTARG]=%s\n",ANTARG,argv[ANTARG]);	*/
-/* Om usr (userid) bˆrjar pÂ 'test' eller 'prov' anv‰nd databas 'olfixtst' */
+/* Om usr (userid) b√∂rjar p√• 'test' eller 'prov' anv√§nd databas 'olfixtst' */
   if (strncmp(usr,"test",4)==0 || strncmp(usr,"prov",4)==0 ) {
   	strncpy(databas,"olfixtst",15);
   }
@@ -108,7 +123,7 @@ int main(int argc, char *argv[], char *envp[])
 
   mysql_init(&my_connection);
 
-  if (mysql_real_connect(&my_connection, "localhost",  "olfix", "olfix", databas, 0, NULL, 0)){
+  if (mysql_real_connect(&my_connection, host,  "olfix", "olfix", databas, 0, NULL, 0)){
 /*  fprintf("VALDEL_Connection success\n");	*/
 
     	res = mysql_query(&my_connection,temp5);
@@ -179,7 +194,7 @@ int which_database(char *envp[])
 	}
 	else{
 /*		fprintf(stderr,"database=%s_len=%d\n",database,strlen(database)); */
-	 	fprintf(stderr,"Error: Filen .olfixrc kan inte ˆppnas\n");
+	 	fprintf(stderr,"Error: Filen .olfixrc kan inte √∂ppnas\n");
 	}
 	for (i=0;i < strlen(database);i++){
 		tmp[i]=database[i];
@@ -189,6 +204,70 @@ int which_database(char *envp[])
 	strncpy(database,tmp,strlen(tmp));
 	database[strlen(tmp)]=0;
 /*	fprintf(stderr,"databas=%s\n",database);	*/
+
+	return status;
+}
+
+int which_host(char *envp[])
+{
+	FILE *fil_pek;
+
+	char home[50];
+	char *home_pek;
+	char resource[]="/.olfixrc";
+	char filename[50]="";
+	char tmp[20]="";
+	char temp[10]="";
+	char *tmp_pek;
+	int i,status;
+	
+	strncpy(host,"localhost",10);		/* default, √∂verskrivs om .olfirc inneh√•ller en hostadr */
+
+	for (i = 0;envp[i]!=NULL;i++){
+		if(strstr(envp[i],"HOME=") != NULL){
+			strncpy(temp,envp[i],4);
+/*			fprintf(stderr,"temp=%s\n",temp); */
+			status=strcmp(temp,"HOME");
+/*			fprintf(stderr,"status=%d\n",status); */
+			if (status == 0){
+				home_pek=(strstr(envp[i],"HOME="));
+				home_pek=home_pek+5;
+				strcpy(home,home_pek);
+			}
+/*			fprintf(stderr,"home_pek=%d %s\n",home_pek,home_pek);	*/
+		}
+	}
+/*	fprintf(stderr,"home=%s\n",home);	*/
+	strncpy(filename,home,strlen(home));
+	strncat(filename,resource,strlen(resource));
+
+/*	fprintf(stderr,"filename=%s\n",filename);	*/
+	status=-1;
+
+	if ((fil_pek = fopen(filename,"r")) != NULL){
+		while (fgets(tmp,150,fil_pek) != NULL){
+/*			fprintf(stderr,"tmp=%s\n",tmp); 	*/
+			if(strstr(tmp,"HOST=")){
+				tmp_pek=(strstr(tmp,"HOST="))+5;
+				strncpy(host,tmp_pek,strlen(tmp_pek));
+				status=0;
+			}
+		}
+/*		fprintf(stderr," Host=%s_len=%d\n",host,strlen(host)); 	*/
+		fclose(fil_pek);
+	}
+	else{
+/*		fprintf(stderr," Host=%s_len=%d\n",host,strlen(host)); 	*/
+	 	fprintf(stderr,"Error: Filen .olfixrc kan inte √∂ppnas\n");
+	}
+	for (i=0;i < strlen(host);i++){
+		tmp[i]=host[i];
+	}
+	tmp[i-1]=0;
+/*	fprintf(stderr,"tmp=%s, i=%d len(tmp)=%d\n",tmp,i,strlen(tmp));	*/
+	strncpy(host,tmp,strlen(tmp));
+	host[strlen(tmp)]=0;
+/*	fprintf(stderr,"host=%s\n",host);	*/
 
 	return status;
 }
